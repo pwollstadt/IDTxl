8fba0dadabd5c41e4a3b417876ca30ea27775034
trentool <p.wollstadt@stud.uni-frankfurt.de>
2015-07-23 14:32:55 +0200

    FIX: dims of points in lists are now read out correctly

--- trentoolxl/estimators_cmi.py
+++ trentoolxl/estimators_cmi.py
@@ -1,7 +1,7 @@
 from jpype import *
 import pyinfo
 
-def jidt_kraskov(self, source, target, conditional, knn):
+def jidt_kraskov(self, var1, var2, conditional, knn):
     """Return the conditional mutual information calculated by JIDT using the
     Kraskov estimator."""
     
@@ -12,14 +12,15 @@ def jidt_kraskov(self, source, target, conditional, knn):
     calc = calcClass()
     calc.setProperty("NORMALISE", "true")
     calc.setProperty("k", str(knn))
-    calc.initialise(source.size[1], target.size[1], conditional.size[1])
-    calc.setObservations(JArray(JDouble, 2)(source), 
-                         JArray(JDouble, 2)(target),
+    #calc.initialise(var1.size[1], var2.size[1], conditional.size[1])
+    calc.initialise(len(var1[1]),len(var2[1]),len(conditional[1]))
+    calc.setObservations(JArray(JDouble, 2)(var1), 
+                         JArray(JDouble, 2)(var2),
                          JArray(JDouble, 2)(conditional))
     return calc.computeAverageLocalOfObservations()
 
-def pyinfo_kraskov(self, source, target, conditional, knn):
+def pyinfo_kraskov(self, var1, var2, conditional, knn):
     """Return the conditional mutual information calculated by the pyinfo module
     using the Kraskov estimator."""
     
-    return pyinfo.cmi_kraskov(source, target, conditional)
+    return pyinfo.cmi_kraskov(var1, var2, conditional)
