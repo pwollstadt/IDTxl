4f61ce68485ebf31f3c691cd2792eebde450ec96
trentool <p.wollstadt@stud.uni-frankfurt.de>
2015-07-23 12:46:35 +0200

    fixed test scripts for mi and cmi estimation in JIDT

--- dev/test_jidt/estimate_cmi_jidt.py
+++ dev/test_jidt/estimate_cmi_jidt.py
@@ -9,16 +9,23 @@ if not isJVMStarted():
 # generate data, taken from the TE example
 numObservations = 1000
 covariance=0.4
+dim = 5
 source = [random.normalvariate(0,1) for r in range(numObservations)]
 target = [0] + [sum(pair) for pair in zip([covariance*y for y in source[0:numObservations-1]], \
                   [(1-covariance)*y for y in [random.normalvariate(0,1) for r in range(numObservations-1)]] ) ]
 conditional = [random.normalvariate(0,1) for r in range(numObservations)]
 
+source = [[random.normalvariate(0,1) for x in range(dim)] for x in range(numObservations)]
+target = [[random.normalvariate(0,1) for x in range(dim)] for x in range(numObservations)]
+conditional = [[random.normalvariate(0,1) for x in range(dim)] for x in range(numObservations)]
+
 cmiCalcClass = JPackage("infodynamics.measures.continuous.kraskov").ConditionalMutualInfoCalculatorMultiVariateKraskov2
 cmiCalc = cmiCalcClass()
-cmiCalc.initialise(1, 1, 1)               # needs the dimensions of all three vars
-cmiCalc.setObservations(JArray(JDouble, 1)(source), 
-                        JArray(JDouble, 1)(target),
-                        JArray(JDouble, 1)(conditional))
+cmiCalc.setProperty("NORMALISE", "true")
+cmiCalc.setProperty("k", "4")
+cmiCalc.initialise(dim,dim,dim)               # needs the dimensions of all three vars
+cmiCalc.setObservations(JArray(JDouble, 2)(source), 
+                        JArray(JDouble, 2)(target),
+                        JArray(JDouble, 2)(conditional))
 cmi = cmiCalc.computeAverageLocalOfObservations()
 print("result: %.4f nats." % cmi)
