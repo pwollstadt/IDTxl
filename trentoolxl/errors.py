b1b3f5ed462e968b6b3987161bf43b5040729427
trentool <p.wollstadt@stud.uni-frankfurt.de>
2015-07-27 18:21:01 +0200

    added an error class for missing parameters

--- trentoolxl/errors.py
+++ trentoolxl/errors.py
@@ -1,7 +1,13 @@
 # https://docs.python.org/3/tutorial/errors.html#user-defined-exceptions
+# http://stackoverflow.com/questions/1319615/proper-way-to-declare-custom-exceptions-in-modern-python
 
 class IdtxlError(Exception):
     def __init__(self, value):
         self.value = value
     def __str__(self):
-    return repr(self.value)
+        return repr(self.value)
+
+class IdtxlParamError(Exception):
+    def __init__(self, value, missing_param):
+        self.value = value
+        self.missing_param = missing_param
