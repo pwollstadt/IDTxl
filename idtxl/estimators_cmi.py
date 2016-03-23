2de3105a7e7754a673cf3068c4401009e2bc926a
Michael Wibral <wibral@uni-frankfurt.de>
2016-03-24 00:47:35 +1100

    added stump for cmi estimator (opencl)

--- idtxl/estimators_cmi.py
+++ idtxl/estimators_cmi.py
@@ -1,13 +1,114 @@
 import jpype as jp
 import pyinfo
 import utils as utils
+import neighbour_search_opencl as nsocl
+from scipy.special import digamma
+
+
+def opencl_kraskov(self, var1, var2, conditional, opts=None):
+    """Calculate conditional mutual information with an opencl-based Kraskov
+        type 1 estimator.
+
+    Calculate the conditional mutual information between three variables.
+    Uses the Kraskov type1 estimator. 
+    References:
+
+        Kraskov, A., Stögbauer, H., & Grassberger, P. (2004). Estimating mutual
+        information. Physical review E, 69(6), 066138.
+
+    This function is ment to be imported into the set_estimator module and used
+    as a method in the Estimator_cmi class.
+
+    Args:
+        self (Estimator_cmi): instance of Estimator_cmi
+        var1: numpy array with realisations of the first random variable, where
+            dimensions are realisations x variable dimension
+        var2: numpy array with realisations of the second random variable
+        conditional: numpy array with realisations of the random variable for
+            conditioning
+        opts : dict [optional]
+            sets estimation parameters:
+            'kraskov_k' - no. nearest neighbours for KNN search (default=4)
+            'theiler_t' - no. next temporal neighbours ignored in KNN and
+            range searches (default='ACT', the autocorr. time of the target)
+            'noise_level' - random noise added to the data (default=1e-8)
+
+    Returns:
+        conditional mutual information
+
+    Note:
+        The Theiler window ignores
+        trial boundaries. The CMI estimator does add noise to the data as a
+        default. To make analysis runs replicable set noise_level to 0.
+    """
+    if opts is None:
+        opts = {}
+    try:
+        kraskov_k = int(opts['kraskov_k'])
+    except KeyError:
+        kraskov_k = int(4)
+    try:
+        theiler_t = int(opts['theiler_t'])
+    except KeyError:
+        theiler_t = int(0)
+    try:
+        noise_level = int(opts['noise_level'])
+    except KeyError:
+        noise_level = (1e-8).float32
+    try:
+        gpuid = int(opts['gpuid'])
+    except KeyError:
+        gpuid = int(0)
+
+    try:
+        nchunkspergpu = int(opts['nchunkspergpu'])
+    except KeyError:
+        nchunkspergpu = int(1)
+
+    # build pointsets - Note we assume that pointsets are given in IDTxl conv.
+    # 1. full space
+    pointset_full_space = np.hstack((var1, var2, conditional))
+    pointset_full_space = pointset_full_space.astype('float32')
+    # 2. conditional variable only
+    pointset_conditional = np.array(conditional)
+    pointset_conditional = pointset_conditional.astype('float32')
+    # 3. pointset variable 1 and conditional
+    pointset_var1_conditional = np.hstack((var1, conditional))
+    pointset_var1_conditional = pointset_var1_conditional.astype('float32')
+    # 4. pointset variable 2 and conidtional
+    pointset_var2_conditional = np.hstack((var2, conditional))
+    pointset_var2_conditional = pointset_var2_conditional.astype('float32')
+
+
+    signallengthpergpu = pointset_full_space.shape[0]
+    print("working with signallength: %i" %signallengthpergpu)
+    chunksize =signallengthpergpu / nchunkspergpu # TODO check for integer result
+
+    indexes, distances = sgocl.knn_search(pointset_full_space, pointset_full_space, kth, theiler, nchunkspergpu, gpuid)
+##    print("indexes:")
+##    print(indexes)
+##    print("distances")
+##    print(distances)
+##    print("shape of distance matrix: ")
+##    print(distances.shape)
+#    # define the search radii as the distances to the kth (=last) neighbours
+#    radii = distances[distances.shape[0]-1,:]
+##    print(radii)
+#    # get neighbour counts in ranges
+#    # range_search(pointset, queryset, distances, theiler_t, n_chunks=1, gpuid=0)
+#    count_conditional = range_search(pointset_conditional, pointset_conditional, radii, theiler, nchunkspergpu, 0)
+##    print(count_conditional)
+#    count_var1_conditional = range_search(pointset_var1_conditional, pointset_var1_conditional, radii, theiler, nchunkspergpu, 0)
+##    print(count_var1_conditional)
+#    count_var2_conditional = range_search(pointset_var2_conditional, pointset_var2_conditional, radii, theiler, nchunkspergpu, 0)
+##
 
 
 def jidt_kraskov(self, var1, var2, conditional, opts=None):
     """Calculate conditional mutual infor with JIDT's Kraskov implementation.
 
     Calculate the conditional mutual information between three variables. Call
-    JIDT via jpype and use the Kraskov 2 estimator. If no conditional is given
+    JIDT via jpype and use the Kraskov 1 estimator. If no conditional is given
     (is None), the function returns the mutual information between var1 and
     var2. References:
 
