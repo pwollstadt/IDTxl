ab2d3e43b17d6e0a81cf56f5037b22af5de71933
pwollstadt <p.wollstadt@stud.uni-frankfurt.de>
2016-04-12 11:55:26 +0200

    wrote a short package description

--- idtxl/__init__.py
+++ idtxl/__init__.py
@@ -1,7 +1,15 @@
+"""IDTxl: Information Dynamics Toolkit xl.
+
+Package provides functionality for information dynamics analysis for arbitrary
+data with an emphasis on the analysis of large data sets and the use of
+multi-node computing clusters.
+
+"""
+
 # __all__ = ["data", "stats", "utils"]
 
 
-#from .data import Data
-#from .multivariate_te import Multivariate_te
+# from .data import Data
+# from .multivariate_te import Multivariate_te
 
 # TODO add everything that should be callable by the user.
