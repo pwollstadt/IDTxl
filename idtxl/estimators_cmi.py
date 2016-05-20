fixed an error in estimators_mi that lead to a computation of the mi for all data in opencl-kraskov, instead of a chunk-wise computation, added mi estimation as a fallback estimation for cmi estimator when no conditional is provided, added assertions ti unit tests that cmi from two chunks of random data is not identical

idtxl/estimators_cmi.py
@@ -9,11 +9,10 @@ import numpy as np
 from scipy.special import digamma
 from . import idtxl_utils as utils
 from . import neighbour_search_opencl as nsocl
-
 VERBOSE = False
 
 
-def opencl_kraskov(self, var1, var2, conditional, opts=None):
+def opencl_kraskov(self, var1, var2, conditional=None, opts=None):
     """Calculate conditional mutual infor using opencl Kraskov implementation.
 
     Calculate the conditional mutual information between three variables using
@@ -76,74 +75,108 @@ def opencl_kraskov(self, var1, var2, conditional, opts=None):
     except KeyError:
         nchunkspergpu = int(1)
 
-    var1 += np.random.normal(size=var1.shape) * noise_level
-    var2 += np.random.normal(size=var2.shape) * noise_level
-    conditional += np.random.normal(size=conditional.shape) * noise_level
+# If no conditional is passed, compute and return the mi:
+# this code is a copy of the one in estimatos_mi look there for comments
+    if conditional == None:
+        if VERBOSE:
+            print('no conditional variable - falling back to MI estimation')
+        var1 += np.random.normal(scale=noise_level, size=var1.shape)
+        var2 += np.random.normal(scale=noise_level, size=var2.shape)
+        pointset_full_space = np.hstack((var1, var2))
+        pointset_full_space = pointset_full_space.astype('float32')
+        n_dim_full = pointset_full_space.shape[1]
+        var1 = var1.astype('float32')
+        n_dim_var1 = var1.shape[1]
+        var2 = var2.astype('float32')
+        n_dim_var2 = var2.shape[1]
+        signallengthpergpu = pointset_full_space.shape[0]
+        chunksize = signallengthpergpu / nchunkspergpu # TODO check for integer result
+        indexes, distances = nsocl.knn_search(pointset_full_space, n_dim_full,
+                                              kraskov_k, theiler_t, nchunkspergpu,
+                                              gpuid)
+        radii = distances[distances.shape[0] - 1, :]
+        count_var1 = nsocl.range_search(var1, n_dim_var1, radii, theiler_t,
+                                        nchunkspergpu, gpuid)
+        count_var2 = nsocl.range_search(var2, n_dim_var2, radii, theiler_t,
+                                        nchunkspergpu, gpuid)
+        cmi_array = -np.inf * np.ones(nchunkspergpu).astype('float64')
+        for chunknum in range(0, nchunkspergpu):
+            mi = (digamma(kraskov_k) + digamma(chunksize) -
+                  np.mean(digamma(count_var1[chunknum * chunksize:
+                                              (chunknum + 1) * chunksize] + 1)
+                                              + digamma(count_var2[chunknum * chunksize:
+                                              (chunknum + 1) * chunksize] + 1)))
+            cmi_array[chunknum] = mi
+    else:
+        var1 += np.random.normal(size=var1.shape) * noise_level
+        var2 += np.random.normal(size=var2.shape) * noise_level
+        conditional += np.random.normal(size=conditional.shape) * noise_level
 
-    # Build pointsets (note that we assume that pointsets are given in IDTxl
-    # convention.
-    # 1. full space
-    pointset_full_space = np.hstack((var1, var2, conditional))
-    pointset_full_space = pointset_full_space.astype('float32')
-    n_dim_full = pointset_full_space.shape[1]
-    # 2. conditional variable only
-    pointset_conditional = np.array(conditional)
-    pointset_conditional = pointset_conditional.astype('float32')
-    n_dim_conditional = pointset_conditional.shape[1]
+        # Build pointsets (note that we assume that pointsets are given in IDTxl
+        # convention.
+        # 1. full space
+        pointset_full_space = np.hstack((var1, var2, conditional))
+        pointset_full_space = pointset_full_space.astype('float32')
+        n_dim_full = pointset_full_space.shape[1]
+        # 2. conditional variable only
+        pointset_conditional = np.array(conditional)
+        pointset_conditional = pointset_conditional.astype('float32')
+        n_dim_conditional = pointset_conditional.shape[1]
+        if VERBOSE:
+            print("n_dim_conditional is: {0}".format(n_dim_conditional))
+        # 3. pointset variable 1 and conditional
+        pointset_var1_conditional = np.hstack((var1, conditional))
+        pointset_var1_conditional = pointset_var1_conditional.astype('float32')
+        n_dim_var1_conditional = pointset_var1_conditional.shape[1]
+        # 4. pointset variable 2 and conditional
+        pointset_var2_conditional = np.hstack((var2, conditional))
+        pointset_var2_conditional = pointset_var2_conditional.astype('float32')
+        n_dim_var2_conditional = pointset_var2_conditional.shape[1]
+
+        signallengthpergpu = pointset_full_space.shape[0]
+
+        if VERBOSE:
+            print('working with signallength: {0}'.format(signallengthpergpu))
+        chunksize = signallengthpergpu / nchunkspergpu
+        assert(int(chunksize)  == chunksize), 'Chunksize is not an interger value.'
+#        assert(type(chunksize) is int), 'Chunksize has to be an integer.'
+        indexes, distances = nsocl.knn_search(pointset_full_space, n_dim_full,
+                                              kraskov_k, theiler_t, nchunkspergpu,
+                                              gpuid)
+        if VERBOSE:
+            print('indexes: {0}\n\ndistances: {1}\n\nshape of distance matrix: '
+                  '{2}'.format(indexes, distances, distances.shape))
+
+        # Define the search radii as the distances to the kth (=last) neighbours
+        radii = distances[distances.shape[0]-1, :]
+        if VERBOSE:
+            print('Radii: {0}'.format(radii))
+
+        # Get neighbour counts in ranges.
+        count_cond = nsocl.range_search(pointset_conditional, n_dim_conditional,
+                                        radii, theiler_t, nchunkspergpu, gpuid)
+        count_var1_cond = nsocl.range_search(pointset_var1_conditional,
+                                             n_dim_var1_conditional, radii,
+                                             theiler_t, nchunkspergpu, gpuid)
+        count_var2_cond = nsocl.range_search(pointset_var2_conditional,
+                                             n_dim_var2_conditional, radii,
+                                             theiler_t, nchunkspergpu, gpuid)
+
+        # Return the results, one cmi per chunk of data.
+        cmi_array = -np.inf * np.ones(nchunkspergpu).astype('float64')
+        for chunknum in range(0, nchunkspergpu):
+            cmi = (digamma(kraskov_k) +
+                   np.mean(digamma(count_cond[chunknum * chunksize:
+                                              (chunknum + 1) * chunksize] + 1) -
+                           digamma(count_var1_cond[chunknum * chunksize:
+                                                   (chunknum + 1) * chunksize] +
+                                   1) -
+                           digamma(count_var2_cond[chunknum * chunksize:
+                                                   (chunknum + 1) * chunksize] + 1)
+                           ))
+            cmi_array[chunknum] = cmi
     if VERBOSE:
-        print("n_dim_conditional is: {0}".format(n_dim_conditional))
-    # 3. pointset variable 1 and conditional
-    pointset_var1_conditional = np.hstack((var1, conditional))
-    pointset_var1_conditional = pointset_var1_conditional.astype('float32')
-    n_dim_var1_conditional = pointset_var1_conditional.shape[1]
-    # 4. pointset variable 2 and conditional
-    pointset_var2_conditional = np.hstack((var2, conditional))
-    pointset_var2_conditional = pointset_var2_conditional.astype('float32')
-    n_dim_var2_conditional = pointset_var2_conditional.shape[1]
-
-    signallengthpergpu = pointset_full_space.shape[0]
-
-    if VERBOSE:
-        print('working with signallength: {0}'.format(signallengthpergpu))
-    chunksize = signallengthpergpu / nchunkspergpu
-    assert(type(chunksize) is int), 'Chunks size has to be an integer.'
-    indexes, distances = nsocl.knn_search(pointset_full_space, n_dim_full,
-                                          kraskov_k, theiler_t, nchunkspergpu,
-                                          gpuid)
-    if VERBOSE:
-        print('indexes: {0}\n\ndistances: {1}\n\nshape of distance matrix: '
-              '{2}'.format(indexes, distances, distances.shape))
-
-    # Define the search radii as the distances to the kth (=last) neighbours
-    radii = distances[distances.shape[0]-1, :]
-    if VERBOSE:
-        print('Radii: {0}'.format(radii))
-
-    # Get neighbour counts in ranges.
-    count_cond = nsocl.range_search(pointset_conditional, n_dim_conditional,
-                                    radii, theiler_t, nchunkspergpu, gpuid)
-    count_var1_cond = nsocl.range_search(pointset_var1_conditional,
-                                         n_dim_var1_conditional, radii,
-                                         theiler_t, nchunkspergpu, gpuid)
-    count_var2_cond = nsocl.range_search(pointset_var2_conditional,
-                                         n_dim_var2_conditional, radii,
-                                         theiler_t, nchunkspergpu, gpuid)
-
-    # Return the results, one cmi per chunk of data.
-    cmi_array = -np.inf * np.ones(nchunkspergpu).astype('float64')
-    for chunknum in range(0, nchunkspergpu):
-        cmi = (digamma(kraskov_k) +
-               np.mean(digamma(count_cond[chunknum * chunksize:
-                                          (chunknum + 1) * chunksize] + 1) -
-                       digamma(count_var1_cond[chunknum * chunksize:
-                                               (chunknum + 1) * chunksize] +
-                               1) -
-                       digamma(count_var2_cond[chunknum * chunksize:
-                                               (chunknum + 1) * chunksize] + 1)
-                       ))
-        cmi_array[chunknum] = cmi
-
-    print('cmi array reads: {0}'.format(cmi_array))
+        print('cmi array reads: {0}'.format(cmi_array))
     return cmi_array
 
 
@@ -250,3 +283,71 @@ def jidt_kraskov(self, var1, var2, conditional, opts=None):
     calc.initialise(var1.shape[1], var2.shape[1], cond_dim)
     calc.setObservations(var1, var2, conditional)
     return calc.computeAverageLocalOfObservations()
+
+#def jidt_discrete(self, var1, var2, conditional, alph1, alph2, alphc, opts=None):
+#    """Calculate conditional mutual infor with JIDT's implementation for discrete
+#    variables"
+#
+#    Calculate the conditional mutual information between three variables. Call
+#    JIDT via jpype and use the discrete estimator.
+#
+#    References
+#    Lizier, Joseph T. (2014). JIDT: an information-theoretic toolkit for
+#    studying the dynamics of complex systems. Front. Robot. AI, 1(11).
+#
+#    This function is ment to be imported into the set_estimator module and used
+#    as a method in the Estimator_cmi class.
+#
+#    Args:
+#        self : instance of Estimator_cmi
+#            function is supposed to be used as part of the Estimator_cmi class
+#        var1 : numpy array of integers
+#            realisations of the first random variable, where dimensions are
+#            realisations x variable dimension
+#        var2 : numpy array of integers
+#            realisations of the second random variable
+#        conditional : numpy array on integers
+#            realisations of the random variable for conditioning
+#        alph1 : alphabet size of var1
+#        alph2 : alphabet size of var2
+#        alphc : alphabet size of conditional variable
+#        opts : dict [optional]
+#            sets estimation parameters:
+#
+#    Returns:
+#        float
+#            conditional mutual information
+#
+#    Note:
+#    """
+#    if opts is None:
+#        opts = {}
+#    try:
+#        num_threads = str(opts['num_threads'])
+#    except KeyError:
+#        num_threads = 'USE_ALL'
+#    try:
+#        debug = opts['debug']
+#    except KeyError:
+#        debug = False
+#    assert(var1.type = 'int....'), 'data received by discrete calculator are not integers'
+#
+#    jarLocation = resource_filename(__name__, 'infodynamics.jar')
+#    if not jp.isJVMStarted():
+#        jp.startJVM(jp.getDefaultJVMPath(), '-ea', ('-Djava.class.path=' +
+#                    jarLocation))
+#
+#    calcClass = (jp.JPackage('infodynamics.measures.discrete.....').
+#                 .....)
+#    calc = calcClass()
+#    calc.setProperty('NUM_THREADS', num_threads)
+#    calc.setDebug(debug)
+#
+#    if conditional is None:
+#        cond_dim = 0
+#    else:
+#        cond_dim = conditional.shape[1]
+#        assert(conditional.size != 0), 'Conditional Array is empty.'
+#    calc.initialise(var1.shape[1], var2.shape[1], cond_dim)
+#    calc.setObservations(var1, var2, conditional)
+#    return calc.computeAverageLocalOfObservations()

idtxl/estimators_mi.py
@@ -99,9 +99,17 @@ def opencl_kraskov(self, var1, var2, opts=None):
                                     nchunkspergpu, gpuid)
     count_var2 = nsocl.range_search(var2, n_dim_var2, radii, theiler_t,
                                     nchunkspergpu, gpuid)
-    mi = (digamma(kraskov_k) + digamma(chunksize) -
-          np.mean(digamma(count_var1 + 1) + digamma(count_var2 + 1)))
-    return mi
+
+    # Return the results, one cmi per chunk of data.
+    mi_array = -np.inf * np.ones(nchunkspergpu).astype('float64')
+    for chunknum in range(0, nchunkspergpu):
+        mi = (digamma(kraskov_k) + digamma(chunksize) -
+              np.mean(digamma(count_var1[chunknum * chunksize:(chunknum + 1) *
+                                         chunksize] + 1) +
+              digamma(count_var2[chunknum * chunksize:(chunknum + 1) *
+                                 chunksize] + 1)))
+        mi_array[chunknum] = mi
+    return mi_array
 
 
 def jidt_kraskov(self, var1, var2, opts=None):

test/test_estimators_cmi.py
@@ -82,7 +82,7 @@ def test_cmi_estimator_ocl():
     """
     # Generate two sets of random normal data, where one set has a given
     # covariance and the second is uncorrelated.
-    n = 4000
+    n = 4001 # This needs to be odd as we loose one sample when shifting signals
     cov = 0.4
     source_1 = [rn.normalvariate(0, 1) for r in range(n)]  # correlated src
     source_2 = [rn.normalvariate(0, 1) for r in range(n)]  # uncorrelated src
@@ -126,7 +126,46 @@ def test_cmi_estimator_ocl():
     assert (np.abs(res_2[0]-res_2[1]) < 0.1), ('CMI calculations for first and'
                                                ' second chunk deviate by more'
                                                'than 0.01')
+    # check that the separate computation for the individual data chunks is
+    # performed, instead of lumping all data together
+    assert(res_1[0] != res_1[1]), ('CMI results for chunk 1 and 2 are'
+                                   'identical, this  is unlikely for random'
+                                   'data.')
+
+def test_cmi_no_c_estimator_ocl():
+    """Tests CMI estimation without a condional variable
+
+    The estimator should fall back to MI estiamtion and provide the correct result
+    """
+    n = 4000
+    cov = 0.4
+    source_1 = [rn.normalvariate(0, 1) for r in range(n)]  # correlated src
+    target = [sum(pair) for pair in zip(
+        [cov * y for y in source_1],
+        [(1 - cov) * y for y in [rn.normalvariate(0, 1) for r in range(n)]])]
+    # Cast everything to numpy so the idtxl estimator understands it.
+    source_1 = np.expand_dims(np.array(source_1), axis=1)
+    target = np.expand_dims(np.array(target), axis=1)
+    # Note that the calculation is a random variable (because the generated
+    # data is a set of random variables) - the result will be of the order of
+    # what we expect, but not exactly equal to it; in fact, there will be a
+    # large variance around it.
+    opts = {'kraskov_k': 4, 'normalise': True, 'nchunkspergpu': 2}
+    calculator_name = 'opencl_kraskov'
+    est = Estimator_cmi(calculator_name)
+    res_1 = est.estimate(var1=source_1[1:], var2=target[1:],
+                         conditional=None, opts=opts)
+    expected_res = math.log(1 / (1 - math.pow(cov, 2)))
+    print('Example 1: TE result for second chunk is {0:.4f} nats;'
+          ' expected to be close to {1:.4f} nats for these correlated'
+          ' Gaussians.'.format(res_1[0], expected_res))
+    assert(res_1[0] != res_1[1]), ('CMI results for chunk 1 and 2 are'
+                                   'identical, this  is unlikely for random'
+                                   'data.')
+
+# TODO: add assertions for the right values
 
 if __name__ == '__main__':
     test_cmi_estimator_jidt_kraskov()
     test_cmi_estimator_ocl()
+    test_cmi_no_c_estimator_ocl()