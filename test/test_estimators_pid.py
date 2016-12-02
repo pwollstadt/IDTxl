71991d7642ea849a6d3f0725fedb56ce6dfe2373
pwollstadt <patricia.wollstadt@gmx.de>
2016-12-02 17:28:57 +0100

    Fix unit test after making PID estimator callable through estimator class

--- test/test_estimators_pid.py
+++ test/test_estimators_pid.py
@@ -1,37 +1,41 @@
-# -*- coding: utf-8 -*-
-"""
+"""Provide unit tests for PID estimators.
+
 Created on Mon Apr 11 21:51:56 2016
 
 @author: wibral
 """
-
 import numpy as np
-from idtxl.estimators_pid import pid
+from idtxl.set_estimator import Estimator_pid
+
 
 def test_logical_xor():
-
+    """Test PID estimation with Sydney estimator on binary XOR."""
     # logical AND
     n = 1000
     alph = 2
     s1 = np.random.randint(0, alph, n)
     s2 = np.random.randint(0, alph, n)
     target = np.logical_xor(s1, s2).astype(int)
-    cfg = {
+    analysis_opts = {
         'alph_s1': 2,
         'alph_s2': 2,
         'alph_t': 2,
-        'jarpath': 'infodynamics.jar',
-        'iterations': 1000
+        'max_unsuc_swaps_row_parm': 3,
+        'num_reps': 63,
+        'max_iters': 10000
     }
-    print('Testing PID estimator on binary AND, pointsset size{0}, iterations: {1}'.format(
-                                                        n, cfg['iterations']))
-    [est, opt] = pid(s1, s2, target, cfg)
+    print('Testing PID estimator on binary XOR, pointsset size{0}, iterations:'
+          ' {1}'.format(n, analysis_opts['max_iters']))
+    pid_est = Estimator_pid('pid_sydney')
+    pid_est = pid_est.estimate(s1, s2, target, analysis_opts)
     print("----Results: ----")
-    print("unq_s1: {0}".format(est['unq_s1']))
-    print("unq_s2: {0}".format(est['unq_s2']))
-    print("shd_s1s2: {0}".format(est['shd_s1s2']))
-    print("syn_s1s2: {0}".format(est['syn_s1s2']))
-    assert 0.9 < est['syn_s1s2'] <=1.1, 'incorrect synergy: {0}, expected was {1}'.format(est['syn_s1s2'], 0.98)
+    print("unq_s1: {0}".format(pid_est['unq_s1']))
+    print("unq_s2: {0}".format(pid_est['unq_s2']))
+    print("shd_s1_s2: {0}".format(pid_est['shd_s1_s2']))
+    print("syn_s1_s2: {0}".format(pid_est['syn_s1_s2']))
+    assert 0.9 < pid_est['syn_s1_s2'] <= 1.1, (
+            'incorrect synergy: {0}, expected was {1}'.format(
+                    pid_est['syn_s1s2'], 0.98))
 
 if __name__ == '__main__':
     test_logical_xor()
