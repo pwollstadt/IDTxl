ac1cae10c7ba61e03b9dffbd2f48eaeba7eb2a45
Patricia Wollstadt <patricia.wollstadt@gmx.de>
2017-05-23 04:37:47 +0200

    Fix example inputs for MuTE system test
    Fix example inputs for the MuTE system test to work with the
    upated interface of the multivariate TE class. Pass analysis
    parameters as keyword arguments.

--- test/systemtest_mute.py
+++ test/systemtest_mute.py
@@ -1,12 +1,11 @@
 import time
 import numpy as np
-from idtxl.multivariate_te import Multivariate_te 
+from idtxl.multivariate_te import Multivariate_te
 from idtxl.data import Data
 
 start_time = time.time()
 dat = Data()  # initialise an empty data object
 dat.generate_mute_data(n_samples=1000, n_replications=10)
-max_lag = 5
 analysis_opts = {
         'cmi_calc_name': 'jidt_kraskov',
         'n_perm_max_stat': 200,
@@ -15,7 +14,10 @@ analysis_opts = {
         'n_perm_max_seq': 500,
         }
 
-network_analysis = Multivariate_te(max_lag, analysis_opts)
+network_analysis = Multivariate_te(
+        max_lag_sources=5,
+        min_lag_sources=1,
+        options=analysis_opts)
 res = network_analysis.analyse_network(dat)
 runtime = time.time() - start_time
 print("---- {0} minutes".format(runtime / 60))
