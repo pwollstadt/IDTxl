cc2b0f7c411f83340c443594d5eba59fd73da303
Janosch Ruff <janosch.ruff@stud.uni-goettingen.de>
2019-05-10 16:09:34 +0200

    adding test if all checkpoints get same result

--- test/test_checkpointing.py
+++ test/test_checkpointing.py
@@ -1,5 +1,6 @@
 """Provide unit tests for IDTxl checkpointing."""
 import os
+import subprocess
 from idtxl.multivariate_te import MultivariateTE
 from idtxl.data import Data
 
@@ -93,7 +94,80 @@ def test_checkpoint_copy():
     os.remove('{0}.dat'.format(filename_ckp))
     os.remove('{0}.json'.format(filename_ckp))
 
+def test_checkpoint_equal():
+    """Assert that a result from a resumed checkpoint is equal to the original start."""
+    filename_ckp = './my_checkpoint'
+    filepath = "test/save_all_checkpoints.sh"
+    # Running Script to save all Checkpoints
+    # PROBLEM: CANNOT START SUBPROCESS PROPERLY
+    #os.system("sh " + filepath + " " + filename_ckp  + ".ckp.old")
+    #subprocess.Popen("sh " + filepath + " " + filename_ckp  + ".ckp.old")
+    subprocess.Popen(['./' + filepath, filename_ckp  + ".ckp.old"])
+    #subprocess.Popen(["bash", "test/save_all_checkpoints.sh")
+    """Test resuming from manually generated checkpoint."""
+    # Generate test data
+    data = Data()
+    data.generate_mute_data(n_samples=1000, n_replications=5)
+
+    # Running Script to save all Checkpoints
+
+    # Initialise analysis object and define settings
+    network_analysis = MultivariateTE()
+    settings = {'cmi_estimator': 'JidtGaussianCMI',
+                'max_lag_sources': 5,
+                'min_lag_sources': 1,
+                'write_ckp': True,
+                'filename_ckp': filename_ckp}
+
+    # Manually create checkpoint files for two targets.
+    # Note that variables are added as absolute time indices and not lags wrt.
+    # the current value. Internally, IDTxl operates with abolute indices, but in
+    # all results and console outputs, variables are described by their lags
+    # wrt. to the current value.
+    sources = [0, 1, 2]
+    targets = [3, 4]
+    add_vars = [[(0, 1), (0, 2), (3, 1)], [(0, 1), (0, 2), (1, 3), (4, 2)]]
+    network_analysis._set_checkpointing_defaults(
+        settings, data, sources, targets)
+
+    for i in range(len(targets)):
+        network_analysis._initialise(settings, data, sources, targets[i])
+        network_analysis.selected_vars_full = add_vars[i]
+        network_analysis._update_checkpoint('{}.ckp'.format(filename_ckp))
+
+    # Resume analysis.
+    network_analysis_res = MultivariateTE()
+    data, settings, targets, sources = network_analysis_res.resume_checkpoint(
+        filename_ckp)
+
+
+    # Test if variables were added correctly
+    for i in range(len(targets)):
+        network_analysis_res._initialise(settings, data, sources, targets[i])
+        assert network_analysis_res.selected_vars_full == add_vars[i], (
+            'Variables were not added correctly for target {0}.'.format(
+                targets[i]))
+
+    results = network_analysis_res.analyse_network(
+        settings=settings, data=data, targets=targets, sources=sources)
+
+    os.system("ps -ef | grep save_all_checkpoints.sh | grep -v grep | awk '{print $2}' | xargs kill")
+
+
+    proc = subprocess.Popen(["ls -dq *my_checkpoint.ckp.old* | wc -l "], stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    checkpoints = int(out)
+
+    os.system("mv my_checkpoint.ckp cmp_checkpoint.ckp")
+    for i in range(1, checkpoints + 1):
+        check = './my_checkpoint.ckp.old'
+
+        data, settings, targets, sources = network_analysis_res.resume_checkpoint(check+str(i))
+        results = network_analysis_res.analyse_network(
+            settings=settings, data=data, targets=targets, sources=sources)
+        os.system("mv my_checkpoint.ckp cmp_checkpoint.ckp" + str(i))
 
 if __name__ == '__main__':
-    test_checkpoint_copy()
-    test_checkpoint_resume()
+    #test_checkpoint_copy()
+    #test_checkpoint_resume()
+    test_checkpoint_equal()
