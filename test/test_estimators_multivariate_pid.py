9f5c73993ea740a39f291b483fa6944ecb03c9c0
Abzinger <abdullah.makkeh@gmail.com>
2020-02-24 19:06:20 +0100

    finished and tested estimators_multivariate_pid

--- test/test_estimators_multivariate_pid.py
+++ test/test_estimators_multivariate_pid.py
@@ -2,7 +2,7 @@
 import time as tm
 import numpy as np
 import pytest
-from idtxl.estimators_pid import SxPID
+from idtxl.estimators_multivariate_pid import SxPID
 
 
 SETTINGS = {}
@@ -21,17 +21,17 @@ def test_goettingen_estimator():
     # Test Shared Goettingen estimator on logical and
     pid_goettingen = SxPID(SETTINGS)
     Z = np.logical_and(X, Y).astype(int)
-    est_goettingen = pid_goettingen.estimate(X, Y, Z)
+    est_goettingen = pid_goettingen.estimate([X, Y], Z)
     assert np.isclose(0.12255624891826572, est_goettingen['avg'][((1,),(2,),)][2]), (
     'Average shared is not 0.1225... for SxPID estimator.' )
     true_values = {
-        (0,0,0): 0.4150374992788438
-        (0,1,0): -0.16992500144231237
-        (1,0,0): -0.16992500144231237
+        (0,0,0): 0.4150374992788438,
+        (0,1,0): -0.16992500144231237,
+        (1,0,0): -0.16992500144231237,
         (1,1,1): 0.4150374992788438
     }
     for i in range(4):
-        assert np.isclose(true_value[(X[i],Y[i],Z[i])],
+        assert np.isclose(true_values[(X[i],Y[i],Z[i])],
                           est_goettingen['ptw'][(X[i],Y[i],Z[i])][((1,),(2,),)][2]), (
                               'pointwise shared at ({0},{1},{2}) is not {3:.8f}'.format(
                                   X[i], Y[i], Z[i],
@@ -52,14 +52,14 @@ def test_pid_xor():
     Z = np.logical_xor(X, Y).astype(int)
     est_goettingen = _estimate(Z)
 
-    assert np.isclose(-0.5849625007211562, est_goettingen['avg'][((1,2,),)][2]), (
+    assert np.isclose(-0.5849625007211562, est_goettingen['avg'][((1,),(2,),)][2]), (
         'Average Shared is not -0.5849...')
 
 
 def test_pid_source_copy():
     """Test PID estimator on copied source."""
     Z = X
-    
+    est_goettingen = _estimate(Z)
 
     assert np.isclose(0.5849625007211562, est_goettingen['avg'][((1,),)][2], atol=1.e-7), (
         'Unique information 1 is not 1 for SxPID estimator ({0}).'.format(
@@ -87,7 +87,7 @@ def test_xor_long():
     # SxPID estimator
     tic = tm.time()
     pid_goettingen = SxPID(SETTINGS)
-    est_goettingen = pid_goettingen.estimate(s1, s2, target)
+    est_goettingen = pid_goettingen.estimate([s1, s2], target)
     t_goettingen = tm.time() - tic
 
     print('\nLogical XOR - N = 1000')
@@ -102,14 +102,9 @@ def test_xor_long():
     print('Synergy s1_s2        {0:.8f}'.format(
                                          est_goettingen['avg'][((1,2,),)][2]))
 
-    assert np.isclose(-0.5849625007211562, est_goettingen['avg'][((1,2,),)][2]), (
+    # atol = 10/n since the divergence from the uniform dist of logic xor is in the order of 10/n
+    assert np.isclose(-0.5849625007211562, est_goettingen['avg'][((1,),(2,),)][2], atol=10/n), (
         'Average Shared is not -0.5849...')
-    # assert 0.9 < est_sydney['syn_s1_s2'] <= 1.1, (
-    #         'Sydney estimator incorrect synergy: {0}, expected was {1}'.format(
-    #                 est_sydney['syn_s1s2'], 0.98))
-    # assert 0.9 < est_tartu['syn_s1_s2'] <= 1.1, (
-    #         'Tartu estimator incorrect synergy: {0}, expected was {1}'.format(
-    #                 est_sydney['syn_s1s2'], 0.98))
 
 
 def _estimate(Z):
@@ -118,7 +113,7 @@ def _estimate(Z):
     # Goettingen estimator
     pid_goettingen = SxPID(SETTINGS)
     tic = tm.time()
-    est_goettingen = pid_goettingen.estimate(X, Y, Z)
+    est_goettingen = pid_goettingen.estimate([X, Y], Z)
     t_goettingen = tm.time() - tic
 
     print('\nCopied source')
@@ -143,7 +138,7 @@ def test_int_types():
     print(type(Z[0]))
     est_goettingen = _estimate(Z)
 
-    assert np.isclose(-0.5849625007211562, est_goettingen['avg'][((1,2,),)][2]), (
+    assert np.isclose(-0.5849625007211562, est_goettingen['avg'][((1,),(2,),)][2]), (
         'Average Shared is not -0.5849...')
 
     Z = np.logical_xor(X, Y).astype(np.int64)
@@ -152,7 +147,7 @@ def test_int_types():
     print(type(Z[0]))
     est_goettingen = _estimate(Z)
 
-    assert np.isclose(-0.5849625007211562, est_goettingen['avg'][((1,2,),)][2]), (
+    assert np.isclose(-0.5849625007211562, est_goettingen['avg'][((1,),(2,),)][2]), (
         'Average Shared is not -0.5849...')
 
     Z = [0, 1, 1, 0]
@@ -174,7 +169,7 @@ def test_non_binary_alphabet():
 
     # Goettingen SxPID estimator
     tic = tm.time()
-    est_goettingen = pid_goettingen.estimate(s1, s2, target)
+    est_goettingen = pid_goettingen.estimate([s1, s2], target)
     t_goettingen = tm.time() - tic
 
     print('\nInt addition - N = 1000')
@@ -199,10 +194,75 @@ def test_non_binary_alphabet():
     #                   atol=1e-03), 'Unique2 is not equal.'
 
 
+def test_three_hash():
+    """Test PID estimator on binary three hash."""
+    s1 = np.asarray([0, 0, 0, 0, 1, 1, 1, 1])
+    s2 = np.asarray([0, 0, 1, 1, 0, 0, 1, 1])
+    s3 = np.asarray([0, 1, 0, 1, 0, 1, 0, 1])
+    target = np.logical_xor( s3, np.logical_xor(s1, s2).astype(int) ).astype(int)
+    pid_goettingen = SxPID(SETTINGS)
+    
+    # Goettingen SxPID estimator
+    tic = tm.time()
+    est_goettingen = pid_goettingen.estimate([s1, s2, s3], target)
+    t_goettingen = tm.time() - tic
+
+    print('\n Binary three hash')
+    print('Estimator                                                SxPID\n')
+    print('PID evaluation (Average)                                 {:.3f} s\n'.format(
+        t_goettingen))
+    print('Uni s1                                                   {0:.8f}'.format(
+        est_goettingen['avg'][((1,),)][2]))
+    print('Uni s2                                                   {0:.8f}'.format(
+        est_goettingen['avg'][((2,),)][2]))
+    print('Uni s3                                                   {0:.8f}'.format(
+        est_goettingen['avg'][((3,),)][2]))    
+    print('Synergy s1_s2_s3                                         {0:.8f}'.format(
+        est_goettingen['avg'][((1,2,3),)][2]))
+    print('Synergy s1_s2                                            {0:.8f}'.format(
+        est_goettingen['avg'][((1,2,),)][2]))
+    print('Synergy s1_s3                                            {0:.8f}'.format(
+        est_goettingen['avg'][((1,3,),)][2]))
+    print('Synergy s2_s3                                            {0:.8f}'.format(
+        est_goettingen['avg'][((2,3,),)][2]))
+    print('Shared s1_s2_s3                                           {0:.8f}'.format(
+        est_goettingen['avg'][((1,),(2,),(3,),)][2]))
+    print('Shared of (s1, s2)                                       {0:.8f}'.format(
+        est_goettingen['avg'][((1,), (2,),)][2]))
+    print('Shared of (s1, s2)                                       {0:.8f}'.format(
+        est_goettingen['avg'][((1,), (3,),)][2]))
+    print('Shared of (s2, s3)                                       {0:.8f}'.format(
+        est_goettingen['avg'][((2,), (3,),)][2]))
+    print('Shared of (Synergy s1_s2, Synergy s1_s3)                 {0:.8f}'.format(
+        est_goettingen['avg'][((1,2,), (1,3),)][2]))
+    print('Shared of (Synergy s1_s2, Synergy s2_s3)                 {0:.8f}'.format(
+        est_goettingen['avg'][((1,2,), (2,3),)][2]))
+    print('Shared of (Synergy s1_s3, Synergy s2_s3)                 {0:.8f}'.format(
+        est_goettingen['avg'][((1,3,), (2,3),)][2]))
+    print('Shared of (Synergy s1_s2, Synergy s1_s3, Synergy s2_s3)  {0:.8f}'.format(
+        est_goettingen['avg'][((1,2,), (1,3), (2,3),)][2]))
+    print('Shared of (s1, Synergy s2_s3)                            {0:.8f}'.format(
+        est_goettingen['avg'][((1,), (2,3),)][2]))
+    print('Shared of (s2, Synergy s1_s3)                            {0:.8f}'.format(
+        est_goettingen['avg'][((2,), (1,3),)][2]))
+    print('Shared of (s3, Synergy s1_s2)                            {0:.8f}'.format(
+        est_goettingen['avg'][((3,), (1,2),)][2]))
+    
+
+    assert np.isclose(0.1926450779423959, est_goettingen['avg'][((1,),(2,),(3,),)][2]), (
+        'Average Shared is not 0.1926...')
+    assert np.isclose(-0.22686079328030903, est_goettingen['avg'][((1,2,), (1,3), (2,3),)][2]), (
+        'Average Shared of (Synergy s1_s2, Synergy s1_s3, Synergy s2_s3) is not -0.2268...')
+    assert np.isclose(0.24511249783653177, est_goettingen['avg'][((1,2,3),)][2]), (
+        'Average Synergy s1_s2_s3 is not 0.2451...')
+    
+
 if __name__ == '__main__':
+    test_goettingen_estimator()
     test_non_binary_alphabet()
     test_xor_long()
     test_pid_and()
     test_pid_xor()
     test_pid_source_copy()
+    test_three_hash()
     test_int_types()
