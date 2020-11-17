Fix calculation of single-link estimates for multivariate algorithms
Fix calculation of single-link estimates for multivariate algorithms.
Conditioning in calculating output values used only target past states
in multivariate algorithms, instead of the past of all other selected
sources as well. Note that this values is not used by the algorithm, but
is provided in the output for completeness only.

test/test_bivariate_te.py
@@ -6,8 +6,9 @@ import pytest
 import itertools as it
 import numpy as np
 from idtxl.bivariate_te import BivariateTE
+from idtxl.multivariate_te import MultivariateTE
 from idtxl.data import Data
-from idtxl.estimators_jidt import JidtDiscreteCMI, JidtKraskovTE
+from idtxl.estimators_jidt import JidtKraskovTE
 from test_estimators_jidt import jpype_missing
 from test_results import _get_discrete_gauss_data
 from idtxl.idtxl_utils import calculate_mi
@@ -531,7 +532,43 @@ def test_indices_to_lags():
     pass
 
 
+def test_compare_bivariate_and_multivariate_te():
+    """Compare bivariate to multivariate TE estimation."""
+    expected_mi, source, source_uncorr, target = _get_gauss_data(seed=SEED)
+    source = source[1:]
+    source_uncorr = source_uncorr[1:]
+    target = target[:-1]
+    data = Data(np.hstack((source, source_uncorr, target)),
+                dim_order='sp', normalise=False, seed=SEED)
+    settings = {
+        'cmi_estimator': 'JidtKraskovCMI',
+        'n_perm_max_stat': 21,
+        'n_perm_min_stat': 21,
+        'n_perm_max_seq': 21,
+        'n_perm_omnibus': 21,
+        'max_lag_sources': 1,
+        'min_lag_sources': 1,
+        'max_lag_target': 1}
+    nw_bivar = BivariateTE()
+    results = nw_bivar.analyse_single_target(
+        settings, data, target=2, sources=[0, 1])
+    te_bivar = results.get_single_target(2, fdr=False)['te'][0]
+
+    nw_multivar = MultivariateTE()
+    results = nw_multivar.analyse_single_target(
+        settings, data, target=2, sources=[0, 1])
+    te_multivar = results.get_single_target(2, fdr=False)['te'][0]
+
+    print('Estimated TE: {0:0.6f}, estimated TE using multivariate algorithm: '
+          '{1:0.6f} (expected: ~ {2:0.6f}).'.format(
+              te_bivar, te_multivar, expected_mi))
+    assert np.isclose(te_bivar, te_multivar, atol=0.005), (
+        'Estimated TE {0:0.6f} differs from multivariate estimate {1:0.6f} '
+        '(expected: TE {2:0.6f}).'.format(te_bivar, te_multivar, expected_mi))
+
+
 if __name__ == '__main__':
+    test_compare_bivariate_and_multivariate_te()
     test_mute_data()
     test_return_local_values()
     test_gauss_data()