fd9dcb05178365023157afc58e9d1aa7f33258f9
DrMichaelLindner <mlindner.mail@gmx.de>
2021-07-14 19:25:18 +0200

    finalize systemtest_optimization_Rudelt for BBC and shuffling estimators

--- test/systemtest_optimization_Rudelt.py
+++ test/systemtest_optimization_Rudelt.py
@@ -2,25 +2,61 @@
 from idtxl.data_spiketime import Data_spiketime
 from idtxl.optimization_Rudelt import OptimizationRudelt
 
-def test_optimization_Rudelt():
+
+def test_optimization_Rudelt_bbc():
     data = Data_spiketime()             # initialise empty data object
     data.load_Rudelt_data()             # load Rudelt spike time data
 
-    settings = {'embedding_past_range_set': [0.005, 0.00998, 0.01991, 0.03972, 0.07924, 0.15811, 0.31548, 0.62946, 1.25594,
-                                       2.50594, 5.0],
+    settings = {'embedding_past_range_set': [0.005, 0.00998, 0.01991, 0.03972, 0.07924, 0.15811,
+                                             0.31548, 0.62946, 1.25594, 2.50594, 5.0],
                 'number_of_bootstraps_R_max': 10,
                 'number_of_bootstraps_R_tot': 10,
                 'auto_MI_bin_size_set': [0.01, 0.025, 0.05, 0.25],
                 'estimation_method': 'bbc',
                 'debug': True}
+    #settings = {
+    #    'embedding_past_range_set': [0.005, 1.25594,
+    #                                 2.50594, 5.0],
+    #    'embedding_number_of_bins_set': [1, 3, 5],
+    #    'number_of_bootstraps_R_max': 10,
+    #    'number_of_bootstraps_R_tot': 10,
+    #    'auto_MI_bin_size_set': [0.01, 0.025, 0.05, 0.25],
+    #    'estimation_method': 'bbc',
+    #    'debug': True}
 
     processes = [0]
     replications = [0]
     optimization_rudelt = OptimizationRudelt(settings)
-    results = optimization_rudelt.optimize(data, processes, replications)
+    results_bbc = optimization_rudelt.optimize(data, processes, replications)
 
 
+def test_optimization_Rudelt_shuffling():
+    data = Data_spiketime()             # initialise empty data object
+    data.load_Rudelt_data()             # load Rudelt spike time data
+
+    settings = {'embedding_past_range_set': [0.005, 0.00998, 0.01991, 0.03972, 0.07924, 0.15811,
+                                             0.31548, 0.62946, 1.25594, 2.50594, 5.0],
+                'number_of_bootstraps_R_max': 10,
+                'number_of_bootstraps_R_tot': 10,
+                'auto_MI_bin_size_set': [0.01, 0.025, 0.05, 0.25],
+                'estimation_method': 'shuffling',
+                'debug': True}
+    #settings = {
+    #    'embedding_past_range_set': [0.005, 1.25594,
+    #                                 2.50594, 5.0],
+    #    'embedding_number_of_bins_set': [1, 3, 5],
+    #    'number_of_bootstraps_R_max': 10,
+    #    'number_of_bootstraps_R_tot': 10,
+    #    'auto_MI_bin_size_set': [0.01, 0.025, 0.05, 0.25],
+    #    'estimation_method': 'shuffling',
+    #    'debug': True}
+
+    processes = [0]
+    replications = [0]
+    optimization_rudelt = OptimizationRudelt(settings)
+    results_shuffling = optimization_rudelt.optimize(data, processes, replications)
 
 
 if __name__ == '__main__':
-    test_optimization_Rudelt()
+    test_optimization_Rudelt_bbc()
+    test_optimization_Rudelt_shuffling()
