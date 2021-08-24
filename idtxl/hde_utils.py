84d2a396d7b8519e7a6c05d4eaf5c2c389227a4a
DrMichaelLindner <mlindner.mail@gmx.de>
2021-08-24 19:14:07 +0200

    fix and update hd visualization

--- idtxl/hde_utils.py
+++ idtxl/hde_utils.py
@@ -14,6 +14,8 @@ implemented in idtxl by Michael Lindner, Göttingen 2021
 import numpy as np
 from collections import Counter
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from datetime import datetime
 
 
 def get_CI_bounds(R,
@@ -27,7 +29,7 @@ def get_CI_bounds(R,
     interval based on the standard deviation; or an arbitrary
     confidence interval based on percentiles of the replications.
     """
-    
+
     if bootstrap_CI_use_sd:
         sigma_R = np.std(bs_Rs)
         CI_lo = R - 2 * sigma_R
@@ -49,13 +51,13 @@ def get_min_key_for_max_value(d):
     If largest value is attained several times, get the 
     smallest respective key.
     """
-    
+
     sorted_keys = sorted([key for key in d.keys()])
     values = [d[key] for key in sorted_keys]
     max_value = max(values)
     for key, value in zip(sorted_keys, values):
         if value == max_value:
-            return key 
+            return key
 
 
 def get_max_R_T(max_Rs):
@@ -88,14 +90,14 @@ def get_H_spiking(symbol_counts):
     For each symbol, determine what the response was (spike/ no spike),
     and obtain the spiking probability. From that compute the entropy.
     """
-    
+
     number_of_spikes = 0
     number_of_symbols = 0
     for symbol, counts in symbol_counts.items():
         number_of_symbols += counts
         if symbol % 2 == 1:
-            number_of_spikes += counts            
-            
+            number_of_spikes += counts
+
     p_spike = number_of_spikes / number_of_symbols
     return get_shannon_entropy([p_spike,
                                 1 - p_spike])
@@ -161,9 +163,39 @@ def get_past_symbol_counts(symbol_counts, merge=True):
         return past_symbol_counts
 
 
+def make_plot_pretty(ax, ticklabelsize):
+    ax.tick_params(axis='x', top=False)
+    ax.tick_params(axis='y', right=False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    # ax.spines['left'].set_visible(False)
+    ax.get_xaxis().tick_bottom()
+    ax.get_yaxis().tick_left()
+    ax.tick_params(direction='out', width=3, length=10, labelsize=ticklabelsize)
+    for spine in ax.spines.values():
+        spine.set_position(('outward', 5))
+    ax.set_axisbelow(True)
+    ax.grid(axis='y', color='0.8', linestyle='-', linewidth=1)
+
+
+def format_x_label(x, pos):
+    return "{:.0f}".format(x)
+
+
+def format_x_label_in_ms(x, pos):
+    return "{:.0f}".format(x * 1000)
+
+
+def format_y_label(y, pos):
+    if y > 0 and y <= 0.02:
+        ret = "{:.3f}".format(y)
+        return ret[1:]
+    return "{:.2f}".format(y)
+
+
 def hde_visualize_results(results, process, filename=None):
     """
-    This method provides a plot or an output .eps image for the given process containing:
+    This method provides a plot or an output .svg image for the given process containing:
         - optimized values for the process
         - graph for the history dependence
         - graph for auto mutual information (if calculated)
@@ -180,15 +212,25 @@ def hde_visualize_results(results, process, filename=None):
             If filename is not set or None, the result image is shown
 
     Returns:
-        returns a plot or an .eps image with the content mentioned above
+        returns a plot or an .svg image with the content mentioned above
 
     """
     settings = results.settings
     res = results.get_single_process(process)
 
+    titlesize = 24
+    ticklabelsize = 18
+    xlabelsize = 22
+    ylabelsize = 22
+    legendsize = 18
+    date = datetime.today().strftime('%Y-%m-%d')
+    linewidth = 3.0
+    markersize = 9.0
+
     plt.figure(figsize=(18, 18))
     maintitle = "Results of Process " + str(res.Process) + " using estimation method " + settings['estimation_method']
-    plt.suptitle(maintitle, fontsize=25)
+    plt.suptitle(maintitle, fontsize=titlesize, fontweight="bold")
+    # plot table
     plt.subplot(221)
     row_labels = ['$\mathit{T}_{D}$ [s]',
                   '$\\tau_{R}$ [s]',
@@ -226,7 +268,15 @@ def hde_visualize_results(results, process, filename=None):
     the_table.scale(4, 4)
     plt.axis('off')
 
+    # plot date
+    axd = plt.subplot(325)
+    plt.text(0, 0, date, horizontalalignment='center',
+             verticalalignment='center', transform=axd.transAxes, fontsize=xlabelsize)
+    plt.axis('off')
+
+    # plot history dependence
     ax1 = plt.subplot(322)
+    ax1.set_title('History Dependence', fontdict={'fontsize': titlesize, 'fontweight': 'medium'})
     y = np.zeros(len(settings['embedding_past_range_set']))
     count = 0
     for key in settings['embedding_past_range_set']:
@@ -237,36 +287,61 @@ def hde_visualize_results(results, process, filename=None):
         count += 1
 
     x = settings['embedding_past_range_set']
-    ind = np.where(y == 0.000)
-    xs = np.delete(x, ind)
-    ys = np.delete(y, ind)
-    ax1.plot(ys)
-    ax1.axvline(x=(np.abs(ys - res.tau_R)).argmin(), color='k', ls='--', label=r"$\tau_R$ "+str(round(res.tau_R,3)))
-    # ax1.set_xscale('log')
-    xla = [0, len(xs)-1]
-    ax1.set_xticks(xla)
-    ax1.set_xticklabels(xs[xla], fontsize=6, rotation=45)
-    ax1.title.set_text('History Dependence')
-    ax1.legend(loc="lower right")
-    ax1.set_xlabel('past range T [ms]')
-    ax1.set_ylabel('History Dependence R')
+    ax1.plot(x, y, 'b-', linewidth=linewidth)
+    ax1.set_xscale('log')
 
+    ax1.axvline(x=res.tau_R, color='k', ls='--', linewidth=linewidth,
+                label=r"$\tau_R$")
+    # ax1.axvline(x=res.T_D, color='k', ls=':', linewidth=linewidth,
+    #             label=r"$T_D$")
+
+    legend1 = ax1.legend(loc="lower right", fontsize=legendsize)
+    frame = legend1.get_frame()
+    frame.set_facecolor('#d0d0d0')
+    frame.set_edgecolor('#d0d0d0')
+
+    ax1.set_xlabel('past range T [ms]', fontsize=xlabelsize)
+    ax1.set_ylabel('History Dependence R', fontsize=ylabelsize)
+    make_plot_pretty(ax1, ticklabelsize)
+    ax1.xaxis.set_major_formatter(ticker.FuncFormatter(format_x_label_in_ms))
+    ax1.yaxis.set_major_formatter(ticker.FuncFormatter(format_y_label))
+
+    # plot auto MI (if calculated)
     ax2 = plt.subplot(326)
-    ax2.title.set_text('Auto Mutual Information')
+
+    ax2.set_title('Auto Mutual Information', fontdict={'fontsize': titlesize, 'fontweight': 'medium'})
     if 'auto_MI' in res:
+
+        # set line and marker style
+        lin_style = ["-", "--", "-.", ":"]
+        mrk_style = ["v", "^", "o", "*", "h", "s"]
+
         leg2 = []
+        c1 = 0
+        c2 = 0
         for key in res.auto_MI.keys():
             y2 = res.auto_MI[key]
             leg2.append(key)
             x2 = np.linspace(0, settings['auto_MI_max_delay'], len(res.auto_MI[key]))
-            ax2.plot(x2, y2, label=str(int(float(key) * 1000)))
+            ax2.plot(x2, y2,
+                     "b" + lin_style[c1 % len(lin_style)] + mrk_style[c2 % len(mrk_style)],
+                     linewidth=linewidth, markersize=markersize, label=str(int(float(key) * 1000)))
+            c1 += 1
+            c2 += 1
+
         ax2.set_xscale('log')
-        ax2.set_ylabel('normalized Auto MI')
-        ax1.set_xlabel('time t [ms]')
-        ax2.legend(loc="upper right", title="bin size [ms]")
+        ax2.set_ylabel('normalized Auto MI', fontsize=xlabelsize)
+        ax2.set_xlabel('time t [ms]', fontsize=ylabelsize)
+        ax2.xaxis.set_major_formatter(ticker.FuncFormatter(format_x_label_in_ms))
+        make_plot_pretty(ax2, ticklabelsize)
+        legend2 = ax2.legend(loc="upper right", title="bin size [ms]", fontsize=legendsize)
+        legend2.get_title().set_fontsize(legendsize)
+        frame = legend2.get_frame()
+        frame.set_facecolor('#d0d0d0')
+        frame.set_edgecolor('#d0d0d0')
     else:
         plt.text(0.5, 0.5, 'was not calculated', horizontalalignment='center',
-                 verticalalignment='center', transform=ax2.transAxes)
+                 verticalalignment='center', transform=ax2.transAxes, fontsize=titlesize)
         plt.axis('off')
 
     if filename is None:
