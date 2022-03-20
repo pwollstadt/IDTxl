4a90406b2d1be0a737703af80fae0c4e962e0749
David A. Ehrlich <davidalexander.ehrlich@uni-goettingen.de>
2022-03-20 23:14:18 +0100

    Added test_checkpointing_mpi.py

--- test/test_checkpointing_mpi.py
+++ test/test_checkpointing_mpi.py
@@ -1,6 +1,7 @@
 """Provide unit tests for IDTxl checkpointing."""
 import os
 import numpy as np
+import pytest
 from idtxl.multivariate_te import MultivariateTE
 from idtxl.data import Data
 from idtxl.idtxl_utils import calculate_mi
@@ -16,6 +17,8 @@ N_SAMPLES = 1000
 SEED = 0
 GAUSS_COV = 0.4
 DELAY = 1
+
+global_settings = {'MPI': True}
 
 
 def _clear_ckp(filename):
@@ -42,21 +45,27 @@ def test_checkpoint_defaults():
                 'max_lag_sources': 3,
                 'min_lag_sources': 2,
                 'verbose': True,
-                'write_ckp': True}
+                'write_ckp': True,
+                **global_settings}
     sources = [0, 1]
     targets = [3]
     network_analysis = MultivariateTE()
-    network_analysis._set_checkpointing_defaults(settings, data, sources, targets)
+    network_analysis._set_checkpointing_defaults(
+        settings, data, sources, targets)
     ckp_file = os.path.join(os.path.dirname(__file__), 'idtxl_checkpoint')
-    assert os.path.isfile(f'{ckp_file}.ckp'), 'Did not write default checkpoint file'
-    assert os.path.isfile(f'{ckp_file}.dat'), 'Did not write default checkpoint data'
-    assert os.path.isfile(f'{ckp_file}.json'), 'Did not write default checkpoint settings'
+    assert os.path.isfile(
+        f'{ckp_file}.ckp'), 'Did not write default checkpoint file'
+    assert os.path.isfile(
+        f'{ckp_file}.dat'), 'Did not write default checkpoint data'
+    assert os.path.isfile(
+        f'{ckp_file}.json'), 'Did not write default checkpoint settings'
 
     # Check if
     data_res, settings_res, targets_res, sources_res = network_analysis.resume_checkpoint(
         ckp_file)
     for k in settings:
-        assert settings[k] == settings_res[k], f'Unequal entries in settings: key {k}'
+        assert settings[k] == settings_res[
+            k], f'Unequal entries in settings: key {k}'
     assert sources == sources_res, 'Sources not the same'
     assert targets == targets_res, 'Targets not the same'
 
@@ -77,7 +86,8 @@ def test_checkpoint_resume():
                 'max_lag_sources': 3,
                 'min_lag_sources': 2,
                 'write_ckp': True,
-                'filename_ckp': filename_ckp}
+                'filename_ckp': filename_ckp,
+                **global_settings}
 
     # Manually create checkpoint files for two targets.
     # Note that variables are added as absolute time indices and not lags wrt.
@@ -127,7 +137,8 @@ def test_checkpoint_copy():
                 'max_lag_sources': 3,
                 'min_lag_sources': 2,
                 'write_ckp': True,
-                'filename_ckp': filename_ckp}
+                'filename_ckp': filename_ckp,
+                **global_settings}
 
     # Manually create checkpoint files for two targets.
     # Note that variables are added as absolute time indices and not lags wrt.
@@ -169,7 +180,8 @@ def test_JidtGaussianCMI_MMI_checkpoint():
     settings1 = {'cmi_estimator': 'JidtGaussianCMI',
                  'max_lag_sources': 3,
                  'min_lag_sources': 2,
-                 'noise_level': 0
+                 'noise_level': 0,
+                 **global_settings
                  }
 
     # Settings with checkpointing.
@@ -178,7 +190,9 @@ def test_JidtGaussianCMI_MMI_checkpoint():
                  'min_lag_sources': 2,
                  'noise_level': 0,
                  'write_ckp': True,
-                 'filename_ckp': filename_ckp1}
+                 'filename_ckp': filename_ckp1,
+                 **global_settings
+                 }
 
     # Settings resuming from checkpoint.
     settings3 = {'cmi_estimator': 'JidtGaussianCMI',
@@ -186,7 +200,9 @@ def test_JidtGaussianCMI_MMI_checkpoint():
                  'min_lag_sources': 2,
                  'noise_level': 0,
                  'write_ckp': True,
-                 'filename_ckp': filename_ckp2}
+                 'filename_ckp': filename_ckp2,
+                 **global_settings
+                 }
 
     # Setting sources and targets for the analysis
     sources = [0, 1]
@@ -255,7 +271,8 @@ def test_JidtGaussianCMI_MMI_checkpoint():
     print("Comparing the results:")
     assert np.array_equal(result1, result2), 'Result 1 and 2 not equal!'
     assert np.array_equal(result1, result3), 'Result 1 and 3 not equal!'
-    assert not np.array_equal(result1, result4), 'Result 1 and 4 equal, expected to be different!'
+    assert not np.array_equal(
+        result1, result4), 'Result 1 and 4 equal, expected to be different!'
 
     cmp1 = list(set(result1).intersection(result2))
     cmp2 = list(set(result1).intersection(result3))
@@ -446,7 +463,8 @@ def test_JidtDiscreteCMI_MMI_checkpoint():
         'n_perm_max_seq': 30,
         'min_lag_sources': DELAY,
         'max_lag_sources': DELAY,
-        'max_lag_target': 1}
+        'max_lag_target': 1,
+        **global_settings}
 
     settings2 = settings1.copy()
     settings2['write_ckp'] = True
@@ -518,7 +536,8 @@ def test_JidtDiscreteCMI_MMI_checkpoint():
     print("Comparing the results:")
     assert np.array_equal(result1, result2), 'Result 1 and 2 not equal!'
     assert np.array_equal(result1, result3), 'Result 1 and 3 not equal!'
-    assert not np.array_equal(result1, result4), 'Result 1 and 4 equal, expected to be different!'
+    assert not np.array_equal(
+        result1, result4), 'Result 1 and 4 equal, expected to be different!'
 
     cmp1 = list(set(result1).intersection(result2))
     cmp2 = list(set(result1).intersection(result3))
@@ -565,7 +584,8 @@ def test_JidtGaussianCMI_MTE_checkpoint():
                  'n_perm_omnibus': N_PERM,
                  'max_lag_sources': 3,
                  'min_lag_sources': 2,
-                 'noise_level': 0
+                 'noise_level': 0,
+                 **global_settings
                  }
 
     # Settings with checkpointing.
@@ -574,7 +594,9 @@ def test_JidtGaussianCMI_MTE_checkpoint():
                  'min_lag_sources': 2,
                  'noise_level': 0,
                  'write_ckp': True,
-                 'filename_ckp': filename_ckp1}
+                 'filename_ckp': filename_ckp1,
+                 **global_settings
+                 }
 
     # Settings resuming from checkpoint.
     settings3 = {'cmi_estimator': 'JidtGaussianCMI',
@@ -582,7 +604,8 @@ def test_JidtGaussianCMI_MTE_checkpoint():
                  'min_lag_sources': 2,
                  'noise_level': 0,
                  'write_ckp': True,
-                 'filename_ckp': filename_ckp2}
+                 'filename_ckp': filename_ckp2,
+                 **global_settings}
 
     # Setting sources and targets for the analysis
     sources = [0, 1]
@@ -650,7 +673,8 @@ def test_JidtGaussianCMI_MTE_checkpoint():
     print("Comparing the results:")
     assert np.array_equal(result1, result2), 'Result 1 and 2 not equal!'
     assert np.array_equal(result1, result3), 'Result 1 and 3 not equal!'
-    assert not np.array_equal(result1, result4), 'Result 1 and 4 equal, expected to be different!'
+    assert not np.array_equal(
+        result1, result4), 'Result 1 and 4 equal, expected to be different!'
 
     cmp1 = list(set(result1).intersection(result2))
     cmp2 = list(set(result1).intersection(result3))
@@ -822,10 +846,10 @@ def test_JidtDiscreteCMI_MTE_checkpoint():
     """Test multivariate TE estimation from discrete data."""
     # Generate Gaussian test data
     data = _get_discrete_gauss_data(covariance=GAUSS_COV,
-                                n=N_SAMPLES*10,
-                                delay=DELAY,
-                                normalise=False,
-                                seed=SEED)
+                                    n=N_SAMPLES*10,
+                                    delay=DELAY,
+                                    normalise=False,
+                                    seed=SEED)
 
     settings1 = {
         'cmi_estimator': 'JidtDiscreteCMI',
@@ -836,7 +860,9 @@ def test_JidtDiscreteCMI_MTE_checkpoint():
         'n_perm_max_seq': 30,
         'min_lag_sources': DELAY,
         'max_lag_sources': DELAY,
-        'max_lag_target': 1}
+        'max_lag_target': 1,
+        **global_settings
+    }
 
     settings2 = settings1.copy()
     settings2['write_ckp'] = True
@@ -913,7 +939,8 @@ def test_JidtDiscreteCMI_MTE_checkpoint():
     print("Comparing the results:")
     assert np.array_equal(result1, result2), 'Result 1 and 2 not equal!'
     assert np.array_equal(result1, result3), 'Result 1 and 3 not equal!'
-    assert not np.array_equal(result1, result4), 'Result 1 and 4 equal, expected to be different!'
+    assert not np.array_equal(
+        result1, result4), 'Result 1 and 4 equal, expected to be different!'
 
     cmp1 = list(set(result1).intersection(result2))
     cmp2 = list(set(result1).intersection(result3))
@@ -960,7 +987,8 @@ def test_JidtGaussianCMI_BTE_checkpoint():
                  'n_perm_omnibus': N_PERM,
                  'max_lag_sources': 3,
                  'min_lag_sources': 2,
-                 'noise_level': 0
+                 'noise_level': 0,
+                 **global_settings
                  }
 
     # Settings with checkpointing.
@@ -1040,7 +1068,8 @@ def test_JidtGaussianCMI_BTE_checkpoint():
     print("Comparing the results:")
     assert np.array_equal(result1, result2), 'Result 1 and 2 not equal!'
     assert np.array_equal(result1, result3), 'Result 1 and 3 not equal!'
-    assert not np.array_equal(result1, result4), 'Result 1 and 4 equal, expected to be different!'
+    assert not np.array_equal(
+        result1, result4), 'Result 1 and 4 equal, expected to be different!'
 
     cmp1 = list(set(result1).intersection(result2))
     cmp2 = list(set(result1).intersection(result3))
@@ -1199,10 +1228,10 @@ def test_JidtDiscreteCMI_BTE_checkpoint():
     """Test multivariate TE estimation from discrete data."""
     # Generate Gaussian test data
     data = _get_discrete_gauss_data(covariance=GAUSS_COV,
-                                n=N_SAMPLES*10,
-                                delay=DELAY,
-                                normalise=False,
-                                seed=SEED)
+                                    n=N_SAMPLES*10,
+                                    delay=DELAY,
+                                    normalise=False,
+                                    seed=SEED)
 
     settings1 = {
         'cmi_estimator': 'JidtDiscreteCMI',
@@ -1213,7 +1242,9 @@ def test_JidtDiscreteCMI_BTE_checkpoint():
         'n_perm_max_seq': 30,
         'min_lag_sources': DELAY,
         'max_lag_sources': DELAY,
-        'max_lag_target': 1}
+        'max_lag_target': 1,
+        **global_settings
+    }
 
     settings2 = settings1.copy()
     settings2['write_ckp'] = True
@@ -1290,7 +1321,8 @@ def test_JidtDiscreteCMI_BTE_checkpoint():
     print("Comparing the results:")
     assert np.array_equal(result1, result2), 'Result 1 and 2 not equal!'
     assert np.array_equal(result1, result3), 'Result 1 and 3 not equal!'
-    assert not np.array_equal(result1, result4), 'Result 1 and 4 equal, expected to be different!'
+    assert not np.array_equal(
+        result1, result4), 'Result 1 and 4 equal, expected to be different!'
 
     cmp1 = list(set(result1).intersection(result2))
     cmp2 = list(set(result1).intersection(result3))
@@ -1333,7 +1365,8 @@ def test_JidtGaussianCMI_BMI_checkpoint():
     settings1 = {'cmi_estimator': 'JidtGaussianCMI',
                  'max_lag_sources': 3,
                  'min_lag_sources': 2,
-                 'noise_level': 0
+                 'noise_level': 0,
+                 **global_settings
                  }
 
     # Settings with checkpointing.
@@ -1342,7 +1375,8 @@ def test_JidtGaussianCMI_BMI_checkpoint():
                  'min_lag_sources': 2,
                  'noise_level': 0,
                  'write_ckp': True,
-                 'filename_ckp': filename_ckp1}
+                 'filename_ckp': filename_ckp1,
+                 **global_settings}
 
     # Settings resuming from checkpoint.
     settings3 = {'cmi_estimator': 'JidtGaussianCMI',
@@ -1350,7 +1384,8 @@ def test_JidtGaussianCMI_BMI_checkpoint():
                  'min_lag_sources': 2,
                  'noise_level': 0,
                  'write_ckp': True,
-                 'filename_ckp': filename_ckp2}
+                 'filename_ckp': filename_ckp2,
+                 **global_settings}
 
     # Setting sources and targets for the analysis
     sources = [0, 1]
@@ -1419,7 +1454,8 @@ def test_JidtGaussianCMI_BMI_checkpoint():
     print("Comparing the results:")
     assert np.array_equal(result1, result2), 'Result 1 and 2 not equal!'
     assert np.array_equal(result1, result3), 'Result 1 and 3 not equal!'
-    assert not np.array_equal(result1, result4), 'Result 1 and 4 equal, expected to be different!'
+    assert not np.array_equal(
+        result1, result4), 'Result 1 and 4 equal, expected to be different!'
 
     cmp1 = list(set(result1).intersection(result2))
     cmp2 = list(set(result1).intersection(result3))
@@ -1590,10 +1626,10 @@ def test_JidtDiscreteCMI_BMI_checkpoint():
 
     # Generate Gaussian test data
     data = _get_discrete_gauss_data(covariance=GAUSS_COV,
-                                n=N_SAMPLES*10,
-                                delay=DELAY,
-                                normalise=False,
-                                seed=SEED)
+                                    n=N_SAMPLES*10,
+                                    delay=DELAY,
+                                    normalise=False,
+                                    seed=SEED)
 
     settings1 = {
         'cmi_estimator': 'JidtDiscreteCMI',
@@ -1604,7 +1640,9 @@ def test_JidtDiscreteCMI_BMI_checkpoint():
         'n_perm_max_seq': 30,
         'min_lag_sources': DELAY,
         'max_lag_sources': DELAY,
-        'max_lag_target': 1}
+        'max_lag_target': 1,
+        **global_settings
+    }
 
     settings2 = settings1.copy()
     settings2['write_ckp'] = True
@@ -1682,7 +1720,145 @@ def test_JidtDiscreteCMI_BMI_checkpoint():
     print("Comparing the results:")
     assert np.array_equal(result1, result2), 'Result 1 and 2 not equal!'
     assert np.array_equal(result1, result3), 'Result 1 and 3 not equal!'
-    assert not np.array_equal(result1, result4), 'Result 1 and 4 equal, expected to be different!'
+    assert not np.array_equal(
+        result1, result4), 'Result 1 and 4 equal, expected to be different!'
+
+    cmp1 = list(set(result1).intersection(result2))
+    cmp2 = list(set(result1).intersection(result3))
+    cmp3 = list(set(result1).intersection(result4))
+    len1 = len(result1)
+    assert len(cmp1) == len1 and len(cmp2) == len1 and len(cmp3) != len1, (
+        "Discrete MultivariateMI Running with checkpoints does not give the expected results")
+
+    print("Final")
+    print("Elements of comparison between result 1 and 2")
+    print(cmp1)
+    print("Elements of comparison between result 1 and 3")
+    print(cmp2)
+    print("Elements of comparison between result 1 and 4")
+    print(cmp3)
+
+    _clear_ckp(filename_ckp1)
+    _clear_ckp(filename_ckp2)
+
+
+@pytest.mark.mpi
+def test_MPI_checkpoint():
+    return
+    """ run test without checkpointing, with checkpointing without resume and checkpointing with resume to compare the results"""
+    filename_ckp1 = os.path.join(
+        os.path.dirname(__file__), 'data', 'run_checkpoint')
+    filename_ckp2 = os.path.join(
+        os.path.dirname(__file__), 'data', 'resume_checkpoint')
+
+    """Test running analysis without any checkpoint setting."""
+    # Generate test data
+    data = Data(seed=SEED)
+    data.generate_mute_data(n_samples=N_SAMPLES, n_replications=1)
+
+    # Initialise analysis object and define settings
+    network_analysis1 = MultivariateMI()
+    network_analysis2 = MultivariateMI()
+    network_analysis3 = MultivariateMI()
+    network_analysis4 = MultivariateMI()
+
+    # Settings without checkpointing.
+    settings1 = {'cmi_estimator': 'JidtGaussianCMI',
+                 'max_lag_sources': 3,
+                 'min_lag_sources': 2,
+                 'noise_level': 0,
+                 'MPI': True
+                 }
+
+    # Settings with checkpointing.
+    settings2 = {'cmi_estimator': 'JidtGaussianCMI',
+                 'max_lag_sources': 3,
+                 'min_lag_sources': 2,
+                 'noise_level': 0,
+                 'write_ckp': True,
+                 'filename_ckp': filename_ckp1,
+                 'MPI': True
+                 }
+
+    # Settings resuming from checkpoint.
+    settings3 = {'cmi_estimator': 'JidtGaussianCMI',
+                 'max_lag_sources': 3,
+                 'min_lag_sources': 2,
+                 'noise_level': 0,
+                 'write_ckp': True,
+                 'filename_ckp': filename_ckp2,
+                 'MPI': True
+                 }
+
+    # Setting sources and targets for the analysis
+    sources = [0, 1]
+    targets = [2, 3]
+    targets2 = [3]
+
+    # Starting Analysis of the Network
+    # results of a network analysis without checkpointing
+    results1 = network_analysis1.analyse_network(
+        settings=settings1, data=data, targets=targets, sources=sources)
+
+    # results of a network analysis with checkpointing
+    results2 = network_analysis2.analyse_network(
+        settings=settings2, data=data, targets=targets, sources=sources)
+
+    # Creating a checkpoint similar to the above settings where the targets of
+    # of the first source have been already analyzed
+    with open(filename_ckp1 + ".ckp", "r+") as f:
+        tarsource = f.readlines()
+    tarsource = tarsource[8]
+    tarsource = (tarsource[:-1])
+
+    network_analysis3._set_checkpointing_defaults(
+        settings3, data, sources, targets)
+
+    with open(filename_ckp2 + ".ckp") as f:
+        lines = f.read().splitlines()
+    lines[8] = tarsource
+    with open(filename_ckp2 + ".ckp", 'w') as f:
+        f.write('\n'.join(lines))
+    print(lines)
+
+    # Resume analysis.
+    network_analysis_res = MultivariateMI()
+    data, settings, targets, sources = network_analysis_res.resume_checkpoint(
+        filename_ckp2)
+
+    # results of a network analysis resuming from checkpoint
+    results3 = network_analysis_res.analyse_network(
+        settings=settings3, data=data, targets=targets, sources=sources)
+
+    # results of a network analysis without checkpointing but other targets/sources
+    results4 = network_analysis4.analyse_network(
+        settings=settings1, data=data, targets=targets2, sources=sources)
+
+    adj_matrix1 = results1.get_adjacency_matrix(weights='binary', fdr=False)
+    adj_matrix2 = results2.get_adjacency_matrix(weights='binary', fdr=False)
+    adj_matrix3 = results3.get_adjacency_matrix(weights='binary', fdr=False)
+    adj_matrix4 = results4.get_adjacency_matrix(weights='binary', fdr=False)
+
+    result1 = adj_matrix1.get_edge_list()
+    result2 = adj_matrix2.get_edge_list()
+    result3 = adj_matrix3.get_edge_list()
+    result4 = adj_matrix4.get_edge_list()
+
+    print("Printing results:")
+    print("Result 1: without checkpoint")
+    print(result1)
+    print("Result 2: with checkpoint")
+    print(result2)
+    print("Result 3: resuming from checkpoint")
+    print(result3)
+    print("Result 4: without checkpoint and different targets and sources")
+    print(result4)
+
+    print("Comparing the results:")
+    assert np.array_equal(result1, result2), 'Result 1 and 2 not equal!'
+    assert np.array_equal(result1, result3), 'Result 1 and 3 not equal!'
+    assert not np.array_equal(
+        result1, result4), 'Result 1 and 4 equal, expected to be different!'
 
     cmp1 = list(set(result1).intersection(result2))
     cmp2 = list(set(result1).intersection(result3))
