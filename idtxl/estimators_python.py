3896bc6283d9d144d3eae873c4c818cc251dc698
David A. Ehrlich <davidalexander.ehrlich@uni-goettingen.de>
2023-12-12 14:04:44 +0100

    Added discrete python CMI estimators

--- idtxl/estimators_python.py
+++ idtxl/estimators_python.py
@@ -4,6 +4,7 @@ from scipy.special import digamma
 
 from idtxl.estimator import Estimator
 from idtxl.knn.knn_finder_factory import get_knn_finder
+from idtxl import idtxl_utils as utils
 
 class PythonKraskovCMI(Estimator):
     """Estimate conditional mutual information using Kraskov's first estimator.
@@ -128,3 +129,180 @@ class PythonKraskovCMI(Estimator):
     
     def is_parallel(self):
         return False
+    
+class PythonDiscreteCMI(Estimator):
+
+    def __init__(self, settings=None):
+
+        if settings is None:
+            settings = {}
+
+        self._discretise_method = settings.get('discretise_method', 'none')
+        self._alph1 = settings.get('alph1', 2)
+        self._alph2 = settings.get('alph2', 2)
+        self._alphc = settings.get('alphc', 2)
+
+        self._base = settings.get('base', 2)
+
+    def _discretise_vars(self, var1, var2, conditional=None):
+        # Discretise variables if requested. Otherwise assert data are discrete
+        # and provided alphabet sizes are correct.
+        if self._discretise_method == 'equal':
+            var1 = utils.discretise(var1, self._alph1)
+            var2 = utils.discretise(var2, self._alph2)
+            if conditional is not None:
+                conditional = utils.discretise(conditional, self._alphc)
+
+        elif self._discretise_method == 'max_ent':
+            var1 = utils.discretise_max_ent(var1, self._alph1)
+            var2 = utils.discretise_max_ent(var2, self._alph2)
+            if not (conditional is None):
+                conditional = utils.discretise_max_ent(conditional, self._alphc)
+
+        elif self._discretise_method == 'none':
+            assert issubclass(var1.dtype.type, np.integer), (
+                'Var1 is not an integer numpy array. '
+                'Discretise data to use this estimator.')
+            assert issubclass(var2.dtype.type, np.integer), (
+                'Var2 is not an integer numpy array. '
+                'Discretise data to use this estimator.')
+            assert np.min(var1) >= 0, 'Minimum of var1 is smaller than 0.'
+            assert np.min(var2) >= 0, 'Minimum of var2 is smaller than 0.'
+            assert np.max(var1) < self._alph1, (
+                        'Maximum of var1 is larger than the alphabet size.')
+            assert np.max(var2) < self._alph2, (
+                        'Maximum of var2 is larger than the alphabet size.')
+            if conditional.shape[1] > 0:
+                assert np.min(conditional) >= 0, (
+                        'Minimum of conditional is smaller than 0.')
+                assert issubclass(conditional.dtype.type, np.integer), (
+                    'Conditional is not an integer numpy array. '
+                    'Discretise data to use this estimator.')
+                assert np.max(conditional) < self._alphc, (
+                    'Maximum of conditional is larger than the alphabet size.')
+        else:
+            raise ValueError('Unkown discretisation method.')
+
+        if conditional is not None:
+            return var1, var2, conditional
+        else:
+            return var1, var2
+        
+    def estimate(self, var1: np.ndarray, var2: np.ndarray, conditional=None):
+
+        if conditional is None:
+            conditional = np.empty((len(var1), 0))
+
+        var1 = self._ensure_two_dim_input(var1)
+        var2 = self._ensure_two_dim_input(var2)
+        conditional = self._ensure_two_dim_input(conditional)
+
+        # Discretise if requested.
+        var1, var2, conditional = self._discretise_vars(var1, var2,
+                                                        conditional)
+        
+        
+        
+        # Compute entropies
+        H12c, H1c, H2c, Hc = self._estimate_entropies(var1, var2, conditional)
+
+        # Compute CMI
+        return (H1c + H2c - H12c - Hc)  / np.log(self._base)
+    
+    def is_parallel(self):
+        return False
+    
+    def is_analytic_null_estimator(self):
+        return False
+
+    def _estimate_entropy(self, data: np.ndarray):
+        raise NotImplementedError
+
+class PythonDiscretePluginCMI(PythonDiscreteCMI):
+    """Estimate conditional mutual information using the plugin estimator.
+
+    """
+
+    def __init__(self, settings=None):
+
+        if settings is None:
+            settings = {}
+
+        self._sparsity = settings.get('sparsity', 'auto')
+        assert self._sparsity in ['auto', 'dense', 'sparse'], (
+            'Unknown sparsity option. Choose from "auto", "dense" or "sparse".')
+        self._sparsity_memory_limit = settings.get('memory_limit', 1e9) # 1 GB
+
+        super().__init__(settings)
+
+    def _estimate_entropies(self, V1: np.ndarray, V2: np.ndarray, C: np.ndarray):
+    
+        if self._sparsity == 'auto':
+            # Compute number of bins for dense estimation
+            n_bins = (self._alph1 ** V1.shape[1] * self._alph2 ** V2.shape[1] * self._alphc ** C.shape[1]) * 8
+
+            if n_bins * 8 > self._sparsity_memory_limit:
+                use_sparse = True
+            elif n_bins > len(V1):
+                use_sparse = True
+            else:
+                use_sparse = False
+        else:
+            use_sparse = self._sparsity == 'sparse'
+
+        if use_sparse:
+            return self._estimate_entropies_sparse(V1, V2, C)
+        else:
+            return self._estimate_entropies_dense(V1, V2, C)
+
+
+    def _estimate_entropies_sparse(self, V1: np.ndarray, V2: np.ndarray, C: np.ndarray):
+        """ Estimate entropy using a sparse pmf."""
+
+        N = len(V1)
+        l1 = V1.shape[1]
+        lc = C.shape[1]
+
+        data = np.concatenate((V1, C, V2), axis=1)
+
+        _, counts12c = np.unique(data, axis=0, return_counts=True)
+        _, counts1c = np.unique(data[:, :l1 + lc], axis=0, return_counts=True)
+        _, counts2c = np.unique(data[:, l1:], axis=0, return_counts=True)
+        _, countsc = np.unique(data[:, l1:l1 + lc], axis=0, return_counts=True)
+
+        pmf12c = counts12c / N
+        pmf1c = counts1c / N
+        pmf2c = counts2c / N
+        pmfc = countsc / N
+
+        return  - pmf12c @ np.log(pmf12c), \
+                - pmf1c @ np.log(pmf1c), \
+                - pmf2c @ np.log(pmf2c), \
+                - pmfc @ np.log(pmfc)
+        
+    def _estimate_entropies_dense(self, V1: np.ndarray, V2: np.ndarray, C: np.ndarray):
+        """ Estimate entropy using a dense pmf."""
+
+        N = len(V1)
+        l1 = V1.shape[1]
+        l2 = V2.shape[1]
+        lc = C.shape[1]
+        min_dtype = np.min_scalar_type(N)
+
+        counts12c = np.zeros((self._alph1,) * l1 + (self._alph2,) * l2 + (self._alphc,) * lc, dtype=min_dtype)
+
+        np.add.at(counts12c, tuple(v1 for v1 in V1.T) + tuple(c for c in C.T) + tuple(v2 for v2 in V2.T), 1)
+
+        counts1c = np.sum(counts12c, axis=tuple(a for a in range(l1+lc, l1+lc+l2)), dtype=min_dtype)
+        counts2c = np.sum(counts12c, axis=tuple(a for a in range(0, l1)), dtype=min_dtype)
+        countsc = np.sum(counts1c, axis=tuple(a for a in range(0, l1)), dtype=min_dtype)
+
+        pmf12c = counts12c[counts12c != 0] / N
+        pmf1c = counts1c[counts1c != 0] / N
+        pmf2c = counts2c[counts2c != 0] / N
+        pmfc = countsc[countsc != 0] / N
+
+        return  - pmf12c @ np.log(pmf12c), \
+                - pmf1c @ np.log(pmf1c), \
+                - pmf2c @ np.log(pmf2c), \
+                - pmfc @ np.log(pmfc)
--- test/test_estimators_python.py
+++ test/test_estimators_python.py
@@ -3,8 +3,8 @@ import time
 
 import pytest
 
-from idtxl.estimators_jidt import JidtKraskovCMI
-from idtxl.estimators_python import PythonKraskovCMI
+from idtxl.estimators_jidt import JidtKraskovCMI, JidtDiscreteCMI
+from idtxl.estimators_python import PythonKraskovCMI, PythonDiscretePluginCMI
 
 SEED = 42
 
@@ -164,10 +164,232 @@ def test_mi_gaussian(Sigma):
     print(f'Python MI (sklearn_balltree): {mi_python} (took {itoc - itic} seconds)')
     assert np.isclose(mi_jidt, mi_python, rtol=1e-4, atol=1e-4)
 
+def _create_binary_data(n_samples, st_corr, tc_corr):
+    rng = np.random.default_rng(SEED)
+    T = rng.binomial(1, 0.5, size=(n_samples, 1))
+    S = T ^ rng.binomial(1, 1 - st_corr, size=(n_samples, 1))
+    C = T ^ rng.binomial(1, 1 - tc_corr, size=(n_samples, 1))
+
+    return S, T, C
+
+def test_binary_cmi():
+
+    S, T, C = _create_binary_data(10, 1, .05)
+
+    jidt_estimator = JidtDiscreteCMI()
+    itic = time.perf_counter()
+    cmi_jidt = jidt_estimator.estimate(var1=S, var2=T, conditional=C)
+    itoc = time.perf_counter()
+    print(f'JIDT CMI: {cmi_jidt} (took {itoc - itic} seconds)')
+
+    python_estimator = PythonDiscretePluginCMI({'sparsity':'dense'})
+    itic = time.perf_counter()
+    cmi_python1 = python_estimator.estimate(var1=S, var2=T, conditional=C)
+    itoc = time.perf_counter()
+    print(f'Python CMI (dense): {cmi_python1} (took {itoc - itic} seconds)')
+
+    python_estimator = PythonDiscretePluginCMI({'sparsity':'sparse'})
+    itic = time.perf_counter()
+    cmi_python2 = python_estimator.estimate(var1=S, var2=T, conditional=C)
+    itoc = time.perf_counter()
+    print(f'Python CMI (sparse): {cmi_python2} (took {itoc - itic} seconds)')
+
+    assert np.isclose(cmi_python1, cmi_python2, rtol=1e-100)
+    assert np.isclose(cmi_python1, cmi_jidt, atol=0.0001)
+
+def _create_discrete_data(n_samples, alph1, alph2, alphc, dim1=1, dim2=1, dimc=1):
+    rng = np.random.default_rng(SEED)
+    T = rng.integers(0, alph1, size=(n_samples, dim1))
+    S = rng.integers(0, alph2, size=(n_samples, dim2))
+    C = rng.integers(0, alphc, size=(n_samples, dimc))
+
+    return S, T, C
+
+def test_discrete_cmi():
+    alph1 = 1000
+    alph2 = 1000
+    alphc = 1000
+    S, T, C = _create_discrete_data(1_000_000, alph1, alph2, alphc)
+
+    python_estimator = PythonDiscretePluginCMI({'sparsity':'sparse', 'alph1':alph1, 'alph2':alph2, 'alphc':alphc})
+    itic = time.perf_counter()
+    cmi_python2 = python_estimator.estimate(var1=S, var2=T, conditional=C)
+    itoc = time.perf_counter()
+    print(f'Python CMI (sparse): {cmi_python2} (took {itoc - itic} seconds)')
+
+    python_estimator = PythonDiscretePluginCMI({'sparsity':'dense', 'alph1':alph1, 'alph2':alph2, 'alphc':alphc})
+    itic = time.perf_counter()
+    cmi_python1 = python_estimator.estimate(var1=S, var2=T, conditional=C)
+    itoc = time.perf_counter()
+    print(f'Python CMI (dense): {cmi_python1} (took {itoc - itic} seconds)')
+
+    jidt_estimator = JidtDiscreteCMI({'alph1':alph1, 'alph2':alph2, 'alphc':alphc})
+    itic = time.perf_counter()
+    cmi_jidt = jidt_estimator.estimate(var1=S, var2=T, conditional=C)
+    itoc = time.perf_counter()
+    print(f'JIDT CMI: {cmi_jidt} (took {itoc - itic} seconds)')
+
+    assert np.isclose(cmi_python1, cmi_python2, rtol=1e-100)
+    assert np.isclose(cmi_python1, cmi_jidt, atol=0.0001)
+
+def test_discrete_mi():
+    alph1 = 1000
+    alph2 = 1000
+    S, T, C = _create_discrete_data(10_000, alph1, alph2, 1)
+
+    python_estimator = PythonDiscretePluginCMI({'sparsity':'sparse', 'alph1':alph1, 'alph2':alph2})
+    itic = time.perf_counter()
+    cmi_python2 = python_estimator.estimate(var1=S, var2=T, conditional=None)
+    itoc = time.perf_counter()
+    print(f'Python CMI (sparse): {cmi_python2} (took {itoc - itic} seconds)')
+
+    python_estimator = PythonDiscretePluginCMI({'sparsity':'dense', 'alph1':alph1, 'alph2':alph2})
+    itic = time.perf_counter()
+    cmi_python1 = python_estimator.estimate(var1=S, var2=T, conditional=None)
+    itoc = time.perf_counter()
+    print(f'Python CMI (dense): {cmi_python1} (took {itoc - itic} seconds)')
+
+    jidt_estimator = JidtDiscreteCMI({'alph1':alph1, 'alph2':alph2})
+    itic = time.perf_counter()
+    cmi_jidt = jidt_estimator.estimate(var1=S, var2=T, conditional=None)
+    itoc = time.perf_counter()
+    print(f'JIDT CMI: {cmi_jidt} (took {itoc - itic} seconds)')
+
+    assert np.isclose(cmi_python1, cmi_python2, rtol=1e-100)
+    assert np.isclose(cmi_python1, cmi_jidt, atol=0.0001)
+
+def test_discrete_cmi2d():
+    alph1 = 20
+    alph2 = 20
+    alphc= 20
+    S, T, C = _create_discrete_data(10_000, alph1, alph2, alphc, 2, 2, 2)
+
+    python_estimator = PythonDiscretePluginCMI({'sparsity':'sparse', 'alph1':alph1, 'alph2':alph2, 'alphc':alphc})
+    itic = time.perf_counter()
+    cmi_python2 = python_estimator.estimate(var1=S, var2=T, conditional=C)
+    itoc = time.perf_counter()
+    print(f'Python CMI (sparse): {cmi_python2} (took {itoc - itic} seconds)')
+
+    python_estimator = PythonDiscretePluginCMI({'sparsity':'dense', 'alph1':alph1, 'alph2':alph2, 'alphc':alphc})
+    itic = time.perf_counter()
+    cmi_python1 = python_estimator.estimate(var1=S, var2=T, conditional=C)
+    itoc = time.perf_counter()
+    print(f'Python CMI (dense): {cmi_python1} (took {itoc - itic} seconds)')
+
+    jidt_estimator = JidtDiscreteCMI({'alph1':alph1, 'alph2':alph2, 'alphc':alphc})
+    itic = time.perf_counter()
+    cmi_jidt = jidt_estimator.estimate(var1=S, var2=T, conditional=C)
+    itoc = time.perf_counter()
+    print(f'JIDT CMI: {cmi_jidt} (took {itoc - itic} seconds)')
+
+    assert np.isclose(cmi_python1, cmi_python2, rtol=1e-100)
+    assert np.isclose(cmi_python1, cmi_jidt, atol=0.0001)
+
+def test_discrete_mi2d():
+    alph1 = 100
+    alph2 = 100
+    S, T, C = _create_discrete_data(10_000, alph1, alph2, 1, 2, 2, 2)
+
+    python_estimator = PythonDiscretePluginCMI({'sparsity':'sparse', 'alph1':alph1, 'alph2':alph2})
+    itic = time.perf_counter()
+    cmi_python2 = python_estimator.estimate(var1=S, var2=T, conditional=None)
+    itoc = time.perf_counter()
+    print(f'Python CMI (sparse): {cmi_python2} (took {itoc - itic} seconds)')
+
+    python_estimator = PythonDiscretePluginCMI({'sparsity':'dense', 'alph1':alph1, 'alph2':alph2})
+    itic = time.perf_counter()
+    cmi_python1 = python_estimator.estimate(var1=S, var2=T, conditional=None)
+    itoc = time.perf_counter()
+    print(f'Python CMI (dense): {cmi_python1} (took {itoc - itic} seconds)')
+
+    jidt_estimator = JidtDiscreteCMI({'alph1':alph1, 'alph2':alph2})
+    itic = time.perf_counter()
+    cmi_jidt = jidt_estimator.estimate(var1=S, var2=T, conditional=None)
+    itoc = time.perf_counter()
+    print(f'JIDT CMI: {cmi_jidt} (took {itoc - itic} seconds)')
+
+    assert np.isclose(cmi_python1, cmi_python2, rtol=1e-100)
+    assert np.isclose(cmi_python1, cmi_jidt, atol=0.0001)
+
+@pytest.mark.parametrize('Sigma', _Sigmas_3var)
+def test_discrete_gaussian_cmi(Sigma: np.ndarray):
+    rng = np.random.default_rng(SEED)
+    S, T, C = rng.multivariate_normal(np.zeros(3), Sigma, 10_000).T
+    S, T, C = S[:, np.newaxis], T[:, np.newaxis], C[:, np.newaxis]
+
+    alph1 = 100
+    alph2 = 100
+    alphc = 100
+    kwargs = {'alph1':alph1, 'alph2':alph2, 'alphc':alphc, 'discretise_method':'max_ent'}
+
+    python_estimator = PythonDiscretePluginCMI({'sparsity':'sparse', **kwargs})
+    itic = time.perf_counter()
+    cmi_python2 = python_estimator.estimate(var1=S, var2=T, conditional=None)
+    itoc = time.perf_counter()
+    print(f'Python CMI (sparse): {cmi_python2} (took {itoc - itic} seconds)')
+
+    python_estimator = PythonDiscretePluginCMI({'sparsity':'dense', **kwargs})
+    itic = time.perf_counter()
+    cmi_python1 = python_estimator.estimate(var1=S, var2=T, conditional=None)
+    itoc = time.perf_counter()
+    print(f'Python CMI (dense): {cmi_python1} (took {itoc - itic} seconds)')
+
+    jidt_estimator = JidtDiscreteCMI(kwargs)
+    itic = time.perf_counter()
+    cmi_jidt = jidt_estimator.estimate(var1=S, var2=T, conditional=None)
+    itoc = time.perf_counter()
+    print(f'JIDT CMI: {cmi_jidt} (took {itoc - itic} seconds)')
+
+    assert np.isclose(cmi_python1, cmi_python2, rtol=1e-100)
+    assert np.isclose(cmi_python1, cmi_jidt, atol=0.0001)
+
+@pytest.mark.parametrize('Sigma', _Sigmas_2var)
+def test_discrete_gaussian_mi(Sigma: np.ndarray):
+    rng = np.random.default_rng(SEED)
+    S, T = rng.multivariate_normal(np.zeros(2), Sigma, 10_000).T
+    S, T = S[:, np.newaxis], T[:, np.newaxis]
+
+    alph1 = 100
+    alph2 = 100
+    kwargs = {'alph1':alph1, 'alph2':alph2, 'discretise_method':'max_ent'}
+
+    python_estimator = PythonDiscretePluginCMI({'sparsity':'sparse', **kwargs})
+    itic = time.perf_counter()
+    cmi_python2 = python_estimator.estimate(var1=S, var2=T, conditional=None)
+    itoc = time.perf_counter()
+    print(f'Python CMI (sparse): {cmi_python2} (took {itoc - itic} seconds)')
+
+    python_estimator = PythonDiscretePluginCMI({'sparsity':'dense', **kwargs})
+    itic = time.perf_counter()
+    cmi_python1 = python_estimator.estimate(var1=S, var2=T, conditional=None)
+    itoc = time.perf_counter()
+    print(f'Python CMI (dense): {cmi_python1} (took {itoc - itic} seconds)')
+
+    jidt_estimator = JidtDiscreteCMI(kwargs)
+    itic = time.perf_counter()
+    cmi_jidt = jidt_estimator.estimate(var1=S, var2=T, conditional=None)
+    itoc = time.perf_counter()
+    print(f'JIDT CMI: {cmi_jidt} (took {itoc - itic} seconds)')
+
+    assert np.isclose(cmi_python1, cmi_python2, rtol=1e-100)
+    assert np.isclose(cmi_python1, cmi_jidt, atol=0.0001)
 
 if __name__ == '__main__':
+    print('Testing Kraskov CMI estimator...')
     for sigma in _Sigmas_3var:
         test_cmi_gaussian(sigma)
     for sigma in _Sigmas_2var:
         test_mi_gaussian(sigma)
+    print('All tests passed.')
+
+    print('Testing discrete CMI estimator...')
+    test_binary_cmi()
+    test_discrete_cmi()
+    test_discrete_mi()
+    test_discrete_cmi2d()
+    test_discrete_mi2d()
+    for sigma in _Sigmas_3var:
+        test_discrete_gaussian_cmi(sigma)
+    for sigma in _Sigmas_2var:
+        test_discrete_gaussian_mi(sigma)
     print('All tests passed.')
