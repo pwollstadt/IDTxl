47a0bae3644a2c5433d44c39b3a3656e0b6ccdc2
Patricia Wollstadt <patricia.wollstadt@gmx.de>
2024-01-15 11:23:24 +0100

    Fix PEP8, add documentation, add core estimator demo
    Update documentation and demos for the CMI Kraskov estimators written in
    Python. Format docstrings to work with Sphinx.

--- idtxl/knn/knn_finder_scipy.py
+++ idtxl/knn/knn_finder_scipy.py
@@ -3,26 +3,33 @@ from scipy.spatial import KDTree
 
 from idtxl.knn.tree_knn_finder import TreeKnnFinder
 
-class ScipyKDTreeKnnFinder(TreeKnnFinder):
 
+class ScipyKDTreeKnnFinder(TreeKnnFinder):
     def __init__(self, data: np.ndarray, **kwargs):
         super().__init__(**kwargs)
 
-        if self._metric == 'chebyshev':
+        if self._metric == "chebyshev":
             self._p = np.inf
-        elif self._metric == 'euclidean':
+        elif self._metric == "euclidean":
             self._p = 2
         else:
-            raise ValueError(f'Unsupported metric {self._metric}')
+            raise ValueError(f"Unsupported metric {self._metric}")
 
         self._tree = KDTree(data, leafsize=self._leaf_size)
 
-
     def find_neighbors(self, x: np.ndarray, k: int) -> np.ndarray:
         return self._tree.query(x, k=k, p=np.inf, workers=self._num_threads)
-    
+
     def find_neighbors_within(self, x: np.array, r: float) -> np.ndarray:
-        return self._tree.query_ball_point(x=x, r=np.nextafter(r, 0), p=self._p, workers=self._num_threads)
+        return self._tree.query_ball_point(
+            x=x, r=np.nextafter(r, 0), p=self._p, workers=self._num_threads
+        )
 
     def count_neighbors(self, x: np.ndarray, r: float) -> np.ndarray:
-        return self._tree.query_ball_point(x=x, r=np.nextafter(r, 0), p=self._p, return_length=True, workers=self._num_threads)
+        return self._tree.query_ball_point(
+            x=x,
+            r=np.nextafter(r, 0),
+            p=self._p,
+            return_length=True,
+            workers=self._num_threads,
+        )
